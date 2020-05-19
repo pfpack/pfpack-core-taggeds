0627e3218dc65a04f5e304eb285433bbccbfd9e6
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-05-20 00:10:50 +0300

    Fix UnitExtensions.ToUnit: this

--- src/PrimeFuncPack.Unit/UnitExtensions.cs
+++ src/PrimeFuncPack.Unit/UnitExtensions.cs
@@ -1,15 +1,9 @@
 ﻿#nullable enable
 
-using System.Diagnostics.CodeAnalysis;
-using static PrimeFuncPack.Internals.CodeAnalysis.SuppressMessageCategories;
-using static PrimeFuncPack.Internals.CodeAnalysis.SuppressMessageCheckIds;
-using static PrimeFuncPack.Internals.CodeAnalysis.SuppressMessageJustifications;
-
 namespace PrimeFuncPack
 {
     public static class UnitExtensions
     {
-        [SuppressMessage(category: Style, checkId: RemoveUnusedParameter, Justification = ShippedPublicAPI)]
-        public static Unit ToUnit<TResult>(in TResult result) => default;
+        public static Unit ToUnit<TResult>(this TResult result) => Unit.FromResult(result);
     }
 }
