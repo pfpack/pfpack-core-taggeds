0537542e354b6e2af5ead5a6551c53e146567d61
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-06-07 12:51:04 +0300

    Optional: Implement Filter

---
+++ src/PrimeFuncPack/PrimeFuncPack.Optional/Optional.T.Filter.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+using System;
+
+namespace PrimeFuncPack
+{
+    partial struct Optional<T>
+    {
+        public Optional<T> Filter(in Func<T, bool> predicate)
+            =>
+            IsPresent switch
+            {
+                true when predicate.Invoke(Value) => this,
+                _ => default
+            };
+    }
+}
