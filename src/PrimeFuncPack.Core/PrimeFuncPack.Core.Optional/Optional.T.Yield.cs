6f4457367a179bb1f1c6446909540dc45e73252e
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-06-20 17:06:25 +0300

    Add Optional Yield

---
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.Optional/Optional.T.Yield.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+
+namespace System
+{
+    partial struct Optional<T>
+    {
+        public IEnumerable<T> Yield()
+        {
+            if (box is object)
+            {
+                yield return box;
+            }
+        }
+    }
+}
