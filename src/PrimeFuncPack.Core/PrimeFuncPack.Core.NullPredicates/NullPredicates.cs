12d3024d6ed26399e50ee1654dfa01ebdf0f62c7
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-07-19 13:13:09 +0300

    Format code

--- src/PrimeFuncPack.Core/PrimeFuncPack.Core.NullPredicates/NullPredicates.cs
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.NullPredicates/NullPredicates.cs
@@ -4,20 +4,12 @@ namespace System
 {
     public static class NullPredicates
     {
-        public static bool IsNotNull<T>(in T value)
-            =>
-            value is object;
+        public static bool IsNotNull<T>(in T value) => value is object;
 
-        public static bool IsNotNull<T>(T value)
-            =>
-            IsNotNull(in value);
+        public static bool IsNotNull<T>(T value) => IsNotNull(in value);
 
-        public static bool IsNull<T>(in T value)
-            =>
-            value is null;
+        public static bool IsNull<T>(in T value) => value is null;
 
-        public static bool IsNull<T>(T value)
-            =>
-            IsNull(in value);
+        public static bool IsNull<T>(T value) => IsNull(in value);
     }
 }
