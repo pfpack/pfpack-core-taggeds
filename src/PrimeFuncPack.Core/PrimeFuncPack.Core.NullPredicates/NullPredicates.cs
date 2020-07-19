4436f2d8cc9074e98ca7a5e4eda5999fe42c4786
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-07-19 13:08:41 +0300

    Add in-valued Null predicates overloads

--- src/PrimeFuncPack.Core/PrimeFuncPack.Core.NullPredicates/NullPredicates.cs
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.NullPredicates/NullPredicates.cs
@@ -4,12 +4,20 @@ namespace System
 {
     public static class NullPredicates
     {
-        public static bool IsNotNull<T>(T value)
+        public static bool IsNotNull<T>(in T value)
             =>
             value is object;
 
-        public static bool IsNull<T>(T value)
+        public static bool IsNotNull<T>(T value)
+            =>
+            IsNotNull(in value);
+
+        public static bool IsNull<T>(in T value)
             =>
             value is null;
+
+        public static bool IsNull<T>(T value)
+            =>
+            IsNull(in value);
     }
 }
