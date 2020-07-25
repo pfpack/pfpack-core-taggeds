b16764018f58dc60bfd13f3f3f3cf52b943f7038
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-07-25 11:31:10 +0300

    Implement OnPresent(Async)/InternalOnPresent; Add non-Unit and ValueTask OnPresentAsync

--- src/PrimeFuncPack.Core/PrimeFuncPack.Core.Optional/Optional.T.OnPresent.cs
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.Optional/Optional.T.OnPresent.cs
@@ -6,7 +6,59 @@ namespace System
 {
     partial struct Optional<T>
     {
+        // Parameterized
+
         public Unit OnPresent(in Func<T, Unit> func)
+            =>
+            InternalOnPresent<Unit, Unit>(func, default);
+
+        public Unit OnPresent(Action<T> action)
+            =>
+            InternalOnPresent<Unit, Unit>(present => action.InvokeToUnit(present), default);
+
+        public Task<Unit> OnPresentAsync(in Func<T, Task<Unit>> funcAsync)
+            =>
+            InternalOnPresent<Unit, Task<Unit>>(funcAsync, Task.FromResult<Unit>(default));
+
+        public Task OnPresentAsync(in Func<T, Task> funcAsync)
+            =>
+            InternalOnPresent<Unit, Task>(funcAsync, Task.CompletedTask);
+
+        public ValueTask<Unit> OnPresentAsync(in Func<T, ValueTask<Unit>> funcAsync)
+            =>
+            InternalOnPresent<Unit, ValueTask<Unit>>(funcAsync, default);
+
+        public ValueTask OnPresentAsync(in Func<T, ValueTask> funcAsync)
+            =>
+            InternalOnPresent<Unit, ValueTask>(funcAsync, default);
+
+        // Non-Parameterized
+
+        public Unit OnPresent(in Func<Unit> func)
+            =>
+            InternalOnPresent<Unit, Unit>(func, default);
+
+        public Unit OnPresent(Action action)
+            =>
+            InternalOnPresent<Unit, Unit>(() => action.InvokeToUnit(), default);
+
+        public Task<Unit> OnPresentAsync(in Func<Task<Unit>> funcAsync)
+            =>
+            InternalOnPresent<Unit, Task<Unit>>(funcAsync, Task.FromResult<Unit>(default));
+
+        public Task OnPresentAsync(in Func<Task> funcAsync)
+            =>
+            InternalOnPresent<Unit, Task>(funcAsync, Task.CompletedTask);
+
+        public ValueTask<Unit> OnPresentAsync(in Func<ValueTask<Unit>> funcAsync)
+            =>
+            InternalOnPresent<Unit, ValueTask<Unit>>(funcAsync, default);
+
+        public ValueTask OnPresentAsync(in Func<ValueTask> funcAsync)
+            =>
+            InternalOnPresent<Unit, ValueTask>(funcAsync, default);
+
+        private TOuterResult InternalOnPresent<TResult, TOuterResult>(in Func<T, TOuterResult> func, in TOuterResult defaultOuter)
         {
             if (func is null)
             {
@@ -15,22 +67,12 @@ namespace System
 
             return box switch
             {
-                null => default,
+                null => defaultOuter,
                 var present => func.Invoke(present)
             };
         }
 
-        public Unit OnPresent(Action<T> action)
-        {
-            if (action is null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-
-            return OnPresent(func: present => action.InvokeToUnit(present));
-        }
-
-        public Unit OnPresent(in Func<Unit> func)
+        private TOuterResult InternalOnPresent<TResult, TOuterResult>(in Func<TOuterResult> func, in TOuterResult defaultOuter)
         {
             if (func is null)
             {
@@ -39,46 +81,8 @@ namespace System
 
             return box switch
             {
-                null => default,
+                null => defaultOuter,
                 _ => func.Invoke()
-            };
-        }
-
-        public Unit OnPresent(Action action)
-        {
-            if (action is null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-
-            return OnPresent(func: () => action.InvokeToUnit());
-        }
-
-        public Task<Unit> OnPresentAsync(in Func<T, Task<Unit>> funcAsync)
-        {
-            if (funcAsync is null)
-            {
-                throw new ArgumentNullException(nameof(funcAsync));
-            }
-
-            return box switch
-            {
-                null => Task.FromResult<Unit>(default),
-                var present => funcAsync.Invoke(present)
-            };
-        }
-
-        public Task<Unit> OnPresentAsync(in Func<Task<Unit>> funcAsync)
-        {
-            if (funcAsync is null)
-            {
-                throw new ArgumentNullException(nameof(funcAsync));
-            }
-
-            return box switch
-            {
-                null => Task.FromResult<Unit>(default),
-                _ => funcAsync.Invoke()
             };
         }
     }
