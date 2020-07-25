e3d592b0ce0094e8325224557a1bf8fda3b82b4b
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-07-25 13:01:07 +0300

    Update FlatMap: use default factory instead of default result

--- src/PrimeFuncPack.Core/PrimeFuncPack.Core.Optional/Optional.T.FlatMap.cs
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.Optional/Optional.T.FlatMap.cs
@@ -8,17 +8,17 @@ namespace System
     {
         public Optional<TResult> FlatMap<TResult>(in Func<T, Optional<TResult>> map)
             =>
-            InternalFlatMap<TResult, Optional<TResult>>(map, default);
+            InternalFlatMap<TResult, Optional<TResult>>(map, () => default);
 
         public Task<Optional<TResult>> FlatMapAsync<TResult>(in Func<T, Task<Optional<TResult>>> mapAsync)
             =>
-            InternalFlatMap<TResult, Task<Optional<TResult>>>(mapAsync, Task.FromResult<Optional<TResult>>(default));
+            InternalFlatMap<TResult, Task<Optional<TResult>>>(mapAsync, () => Task.FromResult<Optional<TResult>>(default));
 
         public ValueTask<Optional<TResult>> FlatMapAsync<TResult>(in Func<T, ValueTask<Optional<TResult>>> mapAsync)
             =>
-            InternalFlatMap<TResult, ValueTask<Optional<TResult>>>(mapAsync, default);
+            InternalFlatMap<TResult, ValueTask<Optional<TResult>>>(mapAsync, () => default);
 
-        private TOuterResult InternalFlatMap<TResult, TOuterResult>(in Func<T, TOuterResult> map, in TOuterResult defaultOuter)
+        private TOuterResult InternalFlatMap<TResult, TOuterResult>(in Func<T, TOuterResult> map, in Func<TOuterResult> defaultOuterFactory)
         {
             if (map is null)
             {
@@ -27,7 +27,7 @@ namespace System
 
             return box switch
             {
-                null => defaultOuter,
+                null => defaultOuterFactory.Invoke(),
                 var present => map.Invoke(present)
             };
         }
