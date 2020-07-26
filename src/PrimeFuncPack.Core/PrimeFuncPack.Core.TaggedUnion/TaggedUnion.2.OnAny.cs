71b0371032ae175724852360a2b2d7d0ea4f5606
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-07-26 10:41:28 +0300

    Add OnFirst/Second/Any stubs

---
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.TaggedUnion/TaggedUnion.2.OnAny.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+
+namespace System
+{
+    partial struct TaggedUnion<TTag, TFirst, TSecond>
+    {
+        // TODO: Implement (as wrappers for Optional OnPresent)
+        // TODO: Consider also OnEither and other names
+    }
+}
---
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.TaggedUnion/TaggedUnion.2.OnSecond.cs
@@ -0,0 +1,9 @@
+﻿#nullable enable
+
+namespace System
+{
+    partial struct TaggedUnion<TTag, TFirst, TSecond>
+    {
+        // TODO: Implement (as wrappers for Optional OnPresent)
+    }
+}
