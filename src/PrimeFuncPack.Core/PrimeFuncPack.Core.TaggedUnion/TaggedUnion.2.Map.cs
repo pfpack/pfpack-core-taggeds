036327cd9dd210b41b35a2a02703d298d06fe4ef
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-07-26 22:37:15 +0300

    Adjust ImplMap/Map

--- src/PrimeFuncPack.Core/PrimeFuncPack.Core.TaggedUnion/TaggedUnion.2.Map.cs
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.TaggedUnion/TaggedUnion.2.Map.cs
@@ -7,43 +7,23 @@ namespace System
         public TaggedUnion<TResultFirst, TSecond> MapFirst<TResultFirst>(
             in Func<TFirst, TResultFirst> mapFirst)
             =>
-            Map(
+            ImplMap(
                 mapFirst,
                 second => second);
 
         public TaggedUnion<TFirst, TResultSecond> MapSecond<TResultSecond>(
             in Func<TSecond, TResultSecond> mapSecond)
             =>
-            Map(
+            ImplMap(
                 first => first,
                 mapSecond);
 
         public TaggedUnion<TResultFirst, TResultSecond> Map<TResultFirst, TResultSecond>(
             in Func<TFirst, TResultFirst> mapFirst,
             in Func<TSecond, TResultSecond> mapSecond)
-        {
-            if (mapFirst is null)
-            {
-                throw new ArgumentNullException(nameof(mapFirst));
-            }
-
-            if (mapSecond is null)
-            {
-                throw new ArgumentNullException(nameof(mapSecond));
-            }
-
-            return (boxFirst, boxSecond) switch
-            {
-                (Box<TFirst> presentFirst, _)
-                =>
-                TaggedUnion<TResultFirst, TResultSecond>.CreateFirst(mapFirst(presentFirst)),
-
-                (_, Box<TSecond> presentSecond)
-                =>
-                TaggedUnion<TResultFirst, TResultSecond>.CreateSecond(mapSecond(presentSecond)),
-
-                _ => throw CreateNotInitializedException()
-            };
-        }
+            =>
+            ImplMap(
+                mapFirst,
+                mapSecond);
     }
 }
