bf11dac2430468a0e147c663d4cf8a05334c9986
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-07 19:36:31 +0300

    Refactor: switch with C# 9 logic patterns

--- src/PrimeFuncPack.Core/PrimeFuncPack.Core.Optional/Linq/Internal/InternalLastOrAbsent.IList.cs
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.Optional/Linq/Internal/InternalLastOrAbsent.IList.cs
@@ -8,14 +8,12 @@ namespace System.Linq
     {
         public static Optional<TSource> InternalLastOrAbsent<TSource>(
             this IList<TSource> source)
-        {
-            if (source.Count > 0)
+            =>
+            source.Count switch
             {
-                return Optional.Present(source[^1]);
-            }
-
-            return default;
-        }
+                > 0 => Optional.Present(source[^1]),
+                _ => default
+            };
 
         public static Optional<TSource> InternalLastOrAbsent<TSource>(
             this IList<TSource> source,
