dabd4364cfbc26369210bbb0ad470686839bb155
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-14 22:04:56 +0300

    Simplify ToStringExtensions

--- src/PrimeFuncPack.Core/PrimeFuncPack.Core.ToStringExtensions/ToStringExtensions.cs
+++ src/PrimeFuncPack.Core/PrimeFuncPack.Core.ToStringExtensions/ToStringExtensions.cs
@@ -6,13 +6,6 @@ namespace System
     {
         public static string ToStringOrEmpty<T>(this T value)
             =>
-            value.ToStringOrEmpty(value => value!.ToString());
-
-        public static string ToStringOrEmpty<T>(this T value, in Func<T, string?> fromNotNull)
-        {
-            _ = fromNotNull ?? throw new ArgumentNullException(nameof(fromNotNull));
-
-            return value switch { not null => fromNotNull.Invoke(value).OrEmpty(), _ => string.Empty };
-        }
+            value switch { not null => value.ToString().OrEmpty(), _ => string.Empty };
     }
 }
