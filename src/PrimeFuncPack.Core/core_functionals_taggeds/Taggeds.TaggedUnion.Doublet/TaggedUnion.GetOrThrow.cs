8e0512b5d6b20808936a517fed68522a110bf64d
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-16 22:00:20 +0300

    Remove redundant ImplGetOrThrow

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.TaggedUnion.Doublet/TaggedUnion.GetOrThrow.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.TaggedUnion.Doublet/TaggedUnion.GetOrThrow.cs
@@ -10,7 +10,7 @@ namespace System
 
         public TFirst FirstOrThrow(in Func<Exception> exceptionFactory)
             =>
-            ImplGetOrThrow(WrapFirst, exceptionFactory);
+            WrapFirst().OrThrow(exceptionFactory);
 
         public TSecond SecondOrThrow()
             =>
@@ -18,6 +18,6 @@ namespace System
 
         public TSecond SecondOrThrow(in Func<Exception> exceptionFactory)
             =>
-            ImplGetOrThrow(WrapSecond, exceptionFactory);
+            WrapSecond().OrThrow(exceptionFactory);
     }
 }
