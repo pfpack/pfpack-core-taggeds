a2e0cd3c6f4604d715aa25ad290529392eff0471
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-24 21:19:50 +0300

    Make Result builders classes

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Builders.Implementation/FailureBuilder.T.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Builders.Implementation/FailureBuilder.T.cs
@@ -6,17 +6,13 @@ namespace PrimeFuncPack.Core.Result.Builders
 {
     // Targeted for use in the Result factory methods only
     // Not supposed for explicit use in a client code
-    public readonly partial struct FailureBuilder<TFailure> where TFailure : notnull, new()
+    public sealed class FailureBuilder<TFailure> where TFailure : notnull, new()
     {
-        private readonly object? builderObj;
-
         private readonly TFailure failure;
 
         private FailureBuilder(in TFailure failure)
-        {
-            builderObj = new object();
+            =>
             this.failure = failure;
-        }
 
         // Targeted for use in the Result factory / From wrapper methods only
         internal static FailureBuilder<TFailure> From(in TFailure failure)
@@ -24,10 +20,8 @@ namespace PrimeFuncPack.Core.Result.Builders
             new FailureBuilder<TFailure>(failure ?? throw new ArgumentNullException(nameof(failure)));
 
         // The only member targeted for explicit use in a client code
-        public Result<TSuccess, TFailure> Build<TSuccess>() where TSuccess : notnull => builderObj switch
-        {
-            not null => Result<TSuccess, TFailure>.Failure(failure),
-            _ => throw new InvalidOperationException("The failure builder is not initialized.")
-        };
+        public Result<TSuccess, TFailure> Build<TSuccess>() where TSuccess : notnull
+            =>
+            Result<TSuccess, TFailure>.Failure(failure);
     }
 }
--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Builders.Implementation/SuccessBuilder.T.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Builders.Implementation/SuccessBuilder.T.cs
@@ -6,17 +6,13 @@ namespace PrimeFuncPack.Core.Result.Builders
 {
     // Targeted for use in the Result factory methods only
     // Not supposed for explicit use in a client code
-    public readonly partial struct SuccessBuilder<TSuccess> where TSuccess : notnull
+    public sealed class SuccessBuilder<TSuccess> where TSuccess : notnull
     {
-        private readonly object? builderObj;
-
         private readonly TSuccess success;
 
         private SuccessBuilder(in TSuccess success)
-        {
-            builderObj = new object();
+            =>
             this.success = success;
-        }
 
         // Targeted for use in the Result factory / From wrapper methods only
         internal static SuccessBuilder<TSuccess> From(in TSuccess success)
@@ -24,10 +20,8 @@ namespace PrimeFuncPack.Core.Result.Builders
             new SuccessBuilder<TSuccess>(success ?? throw new ArgumentNullException(nameof(success)));
 
         // The only member targeted for explicit use in a client code
-        public Result<TSuccess, TFailure> Build<TFailure>() where TFailure : notnull, new() => builderObj switch
-        {
-            not null => Result<TSuccess, TFailure>.Success(success),
-            _ => throw new InvalidOperationException("The success builder is not initialized.")
-        };
+        public Result<TSuccess, TFailure> Build<TFailure>() where TFailure : notnull, new()
+            =>
+            Result<TSuccess, TFailure>.Success(success);
     }
 }
