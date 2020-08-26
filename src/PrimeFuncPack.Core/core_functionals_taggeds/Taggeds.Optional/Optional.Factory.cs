9758a0ab9a3dfac7372b8926313b7029e7e43d03
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-26 22:24:50 +0300

    Arrange code

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Optional/Optional.Factory.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Optional/Optional.Factory.cs
@@ -4,13 +4,13 @@ namespace System
 {
     partial class Optional
     {
-        public static Optional<T> Absent<T>()
-            =>
-            Optional<T>.Absent;
-
         public static Optional<T> Present<T>(T value)
             =>
             Optional<T>.Present(value);
+
+        public static Optional<T> Absent<T>()
+            =>
+            Optional<T>.Absent;
 
         public static Optional<T> Wrap<T>(Box<T>? box)
             =>
