914716a6b457ad8a07307ace55da9cb616f7f4d5
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-26 19:50:46 +0300

    Optional: Remove parameterless Fold

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Optional/Optional.T.Fold.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Optional/Optional.T.Fold.cs
@@ -17,17 +17,5 @@ namespace System
         public ValueTask<TResult> FoldAsync<TResult>(Func<T, ValueTask<TResult>> onPresentAsync, Func<ValueTask<TResult>> onAbsentAsync)
             =>
             ImplFold(onPresentAsync, onAbsentAsync);
-
-        public TResult Fold<TResult>(Func<TResult> onPresent, Func<TResult> onAbsent)
-            =>
-            ImplFold(onPresent, onAbsent);
-
-        public Task<TResult> FoldAsync<TResult>(Func<Task<TResult>> onPresentAsync, Func<Task<TResult>> onAbsentAsync)
-            =>
-            ImplFold(onPresentAsync, onAbsentAsync);
-
-        public ValueTask<TResult> FoldAsync<TResult>(Func<ValueTask<TResult>> onPresentAsync, Func<ValueTask<TResult>> onAbsentAsync)
-            =>
-            ImplFold(onPresentAsync, onAbsentAsync);
     }
 }
