df0798b564f806c2c4562ae98e8b1ddd81cdde08
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-26 19:26:29 +0300

    Redesign work with Result internal union

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.T.Sameness.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.T.Sameness.cs
@@ -6,7 +6,7 @@ namespace System
     {
         public static bool Same(Result<TSuccess, TFailure> resultA, Result<TSuccess, TFailure> resultB)
             =>
-            TaggedUnion.Same(resultA.SamenessUnion, resultB.SamenessUnion);
+            TaggedUnion.Same(resultA.SamenessSource, resultB.SamenessSource);
 
         public bool Same(Result<TSuccess, TFailure> other)
             =>
@@ -14,10 +14,10 @@ namespace System
 
         public int GetSamenessHashCode()
             =>
-            HashCode.Combine(SamenessContract, SamenessUnion);
+            HashCode.Combine(SamenessContract, SamenessSource);
 
         private static Type SamenessContract => typeof(Result<TSuccess, TFailure>);
 
-        private TaggedUnion<TSuccess, TFailure> SamenessUnion => unionRaw;
+        private TaggedUnion<TSuccess, TFailure> SamenessSource => union;
     }
 }
---
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/ResultUnionExtensions.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+namespace System
+{
+    internal static class ResultUnionExtensions
+    {
+        public static TaggedUnion<TSuccess, TFailure> OrInited<TSuccess, TFailure>(
+            this in TaggedUnion<TSuccess, TFailure> union)
+            where TSuccess : notnull
+            where TFailure : notnull, new()
+            =>
+            union.Or(() => ResultUnion<TSuccess, TFailure>.Failure(new TFailure()));
+    }
+}
