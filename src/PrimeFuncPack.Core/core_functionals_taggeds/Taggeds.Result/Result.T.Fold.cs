de039274edcfc132fb97969a7d249621bf3fd7af
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-27 21:31:08 +0300

    Result Fold: Add contract checks

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.T.Fold.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.T.Fold.cs
@@ -11,19 +11,28 @@ namespace System
             Func<TSuccess, TResult> mapSuccess,
             Func<TFailure, TResult> mapFailure)
             =>
-            FoldSource.Fold(mapSuccess, mapFailure, AssertInited<TResult>);
+            FoldSource.Fold(
+                mapSuccess ?? throw new ArgumentNullException(nameof(mapSuccess)),
+                mapFailure ?? throw new ArgumentNullException(nameof(mapFailure)),
+                AssertInited<TResult>);
 
         public Task<TResult> FoldAsync<TResult>(
             Func<TSuccess, Task<TResult>> mapSuccessAsync,
             Func<TFailure, Task<TResult>> mapFailureAsync)
             =>
-            FoldSource.FoldAsync(mapSuccessAsync, mapFailureAsync, AssertInited<TResult>);
+            FoldSource.FoldAsync(
+                mapSuccessAsync ?? throw new ArgumentNullException(nameof(mapSuccessAsync)),
+                mapFailureAsync ?? throw new ArgumentNullException(nameof(mapFailureAsync)),
+                AssertInited<TResult>);
 
         public ValueTask<TResult> FoldAsync<TResult>(
             Func<TSuccess, ValueTask<TResult>> mapSuccessAsync,
             Func<TFailure, ValueTask<TResult>> mapFailureAsync)
             =>
-            FoldSource.FoldAsync(mapSuccessAsync, mapFailureAsync, AssertInited<TResult>);
+            FoldSource.FoldAsync(
+                mapSuccessAsync ?? throw new ArgumentNullException(nameof(mapSuccessAsync)),
+                mapFailureAsync ?? throw new ArgumentNullException(nameof(mapFailureAsync)),
+                AssertInited<TResult>);
 
         private TaggedUnion<TSuccess, TFailure> FoldSource => union.OrInited();
 
