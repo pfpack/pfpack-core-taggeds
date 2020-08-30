94828783461c6483b53474b11a318637d691bcfe
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-30 20:30:59 +0300

    Arrage Result factories in according to Optional factories order

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Factory.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Factory.cs
@@ -13,21 +13,5 @@ namespace System
         public static FailureBuilder<TFailure> Failure<TFailure>(TFailure failure) where TFailure : notnull, new()
             =>
             FailureBuilder<TFailure>.Create(failure);
-
-        public static Result<TSuccess, Unit> Present<TSuccess>(TSuccess success) where TSuccess : notnull
-            =>
-            Success(success);
-
-        public static Result<TSuccess, Unit> Absent<TSuccess>() where TSuccess : notnull
-            =>
-            Failure<Unit>(default);
-
-        public static Result<Unit, Unit> True()
-            =>
-            Success<Unit>(default);
-
-        public static Result<Unit, Unit> False()
-            =>
-            Failure<Unit>(default);
     }
 }
