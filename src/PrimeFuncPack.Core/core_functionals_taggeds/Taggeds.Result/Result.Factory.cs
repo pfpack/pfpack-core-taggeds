f1f9c910b36932e97cc103c1c5d143c64b900be5
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-08-30 20:15:18 +0300

    Simplify Result build: remove redundant factory class

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Factory.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Factory.cs
@@ -8,26 +8,26 @@ namespace System
     {
         public static SuccessBuilder<TSuccess> Success<TSuccess>(TSuccess success) where TSuccess : notnull
             =>
-            ResultBuilder.Success(success);
+            SuccessBuilder<TSuccess>.Create(success);
 
         public static FailureBuilder<TFailure> Failure<TFailure>(TFailure failure) where TFailure : notnull, new()
             =>
-            ResultBuilder.Failure(failure);
+            FailureBuilder<TFailure>.Create(failure);
 
         public static Result<TSuccess, Unit> Present<TSuccess>(TSuccess success) where TSuccess : notnull
             =>
-            ResultBuilder.Success(success);
+            Success(success);
 
         public static Result<TSuccess, Unit> Absent<TSuccess>() where TSuccess : notnull
             =>
-            ResultBuilder.Failure<Unit>(default);
+            Failure<Unit>(default);
 
         public static Result<Unit, Unit> True()
             =>
-            ResultBuilder.Success<Unit>(default);
+            Success<Unit>(default);
 
         public static Result<Unit, Unit> False()
             =>
-            ResultBuilder.Failure<Unit>(default);
+            Failure<Unit>(default);
     }
 }
