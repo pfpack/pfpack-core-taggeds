73084b60c61f685b38d2f1644d49874e060c100a
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-09-01 20:11:38 +0300

    Implement Result Flow: Forward / Recover

---
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/IResultFlow.T.Forward.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+
+using static System.Result;
+
+namespace System
+{
+    partial interface IResultFlow<TSuccess, TFailure>
+    {
+        public Result<TNextSuccess, TNextFailure> Forward<TNextSuccess, TNextFailure>(
+            Func<TSuccess, Result<TNextSuccess, TNextFailure>> nextFactory,
+            Func<TFailure, TNextFailure> mapFailure)
+            where TNextSuccess : notnull
+            where TNextFailure : notnull, new()
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+            _ = mapFailure ?? throw new ArgumentNullException(nameof(mapFailure));
+
+            return Current.Fold(nextFactory, failure => Failure(mapFailure.Invoke(failure)));
+        }
+
+        public Result<TNextSuccess, TFailure> Forward<TNextSuccess>(
+            Func<TSuccess, Result<TNextSuccess, TFailure>> nextFactory)
+            where TNextSuccess : notnull
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+
+            return Current.Fold(nextFactory, failure => Failure(failure));
+        }
+
+        public Result<TSuccess, TFailure> Forward(
+            Func<TSuccess, Result<TSuccess, TFailure>> nextFactory)
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+
+            return Current.Fold(nextFactory, _ => Current);
+        }
+    }
+}
--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/IResultFlow.T.Recover.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/IResultFlow.T.Recover.cs
@@ -1,9 +1,38 @@
 ﻿#nullable enable
 
+using static System.Result;
+
 namespace System
 {
     partial interface IResultFlow<TSuccess, TFailure>
     {
-        // TODO: Declare and implement Recover
+        public Result<TNextSuccess, TNextFailure> Recover<TNextSuccess, TNextFailure>(
+            Func<TFailure, Result<TNextSuccess, TNextFailure>> nextFactory,
+            Func<TSuccess, TNextSuccess> mapSuccess)
+            where TNextSuccess : notnull
+            where TNextFailure : notnull, new()
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+            _ = mapSuccess ?? throw new ArgumentNullException(nameof(mapSuccess));
+
+            return Current.Fold(success => Success(mapSuccess.Invoke(success)), nextFactory);
+        }
+
+        public Result<TSuccess, TNextFailure> Recover<TNextFailure>(
+            Func<TFailure, Result<TSuccess, TNextFailure>> nextFactory)
+            where TNextFailure : notnull, new()
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+
+            return Current.Fold(success => Success(success), nextFactory);
+        }
+
+        public Result<TSuccess, TFailure> Recover(
+            Func<TFailure, Result<TSuccess, TFailure>> nextFactory)
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+
+            return Current.Fold(_ => Current, nextFactory);
+        }
     }
 }
