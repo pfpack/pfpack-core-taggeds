87aa656bb80b951c7d8876fa80afcbce62ef4e05
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-09-01 21:54:35 +0300

    Rename: otherFactory

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/IResultFlow.T.Recover.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/IResultFlow.T.Recover.cs
@@ -7,32 +7,32 @@ namespace System
     partial interface IResultFlow<TSuccess, TFailure>
     {
         public Result<TNextSuccess, TNextFailure> Recover<TNextSuccess, TNextFailure>(
-            Func<TFailure, Result<TNextSuccess, TNextFailure>> nextFactory,
+            Func<TFailure, Result<TNextSuccess, TNextFailure>> otherFactory,
             Func<TSuccess, TNextSuccess> mapSuccess)
             where TNextSuccess : notnull
             where TNextFailure : notnull, new()
         {
-            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+            _ = otherFactory ?? throw new ArgumentNullException(nameof(otherFactory));
             _ = mapSuccess ?? throw new ArgumentNullException(nameof(mapSuccess));
 
-            return Current.Fold(success => Success(mapSuccess.Invoke(success)), nextFactory);
+            return Current.Fold(success => Success(mapSuccess.Invoke(success)), otherFactory);
         }
 
         public Result<TSuccess, TNextFailure> Recover<TNextFailure>(
-            Func<TFailure, Result<TSuccess, TNextFailure>> nextFactory)
+            Func<TFailure, Result<TSuccess, TNextFailure>> otherFactory)
             where TNextFailure : notnull, new()
         {
-            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+            _ = otherFactory ?? throw new ArgumentNullException(nameof(otherFactory));
 
-            return Current.Fold(success => Success(success), nextFactory);
+            return Current.Fold(success => Success(success), otherFactory);
         }
 
         public Result<TSuccess, TFailure> Recover(
-            Func<TFailure, Result<TSuccess, TFailure>> nextFactory)
+            Func<TFailure, Result<TSuccess, TFailure>> otherFactory)
         {
-            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+            _ = otherFactory ?? throw new ArgumentNullException(nameof(otherFactory));
 
-            return Current.Fold(_ => Current, nextFactory);
+            return Current.Fold(_ => Current, otherFactory);
         }
     }
 }
