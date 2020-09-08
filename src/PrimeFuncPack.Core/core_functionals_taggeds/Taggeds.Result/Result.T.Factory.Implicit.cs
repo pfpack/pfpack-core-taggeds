77f7ad972993c025b70998d70f18ff54e202bd3c
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-09-08 18:55:53 +0300

    Add implicit Result factories

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.T.Factory.Implicit.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.T.Factory.Implicit.cs
@@ -6,6 +6,14 @@ namespace System
 {
     partial struct Result<TSuccess, TFailure>
     {
+        public static implicit operator Result<TSuccess, TFailure>(in TSuccess success)
+            =>
+            Success(success);
+
+        public static implicit operator Result<TSuccess, TFailure>(in TFailure failure)
+            =>
+            Failure(failure);
+
         public static implicit operator Result<TSuccess, TFailure>(in SuccessBuilder<TSuccess> success)
             =>
             success.Build<TFailure>();
