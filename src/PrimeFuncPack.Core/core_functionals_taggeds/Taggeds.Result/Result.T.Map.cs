d8b93eb9965f5f444da3b0137dddf0a62b990189
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-09-11 01:12:49 +0300

    Result: Add Map async

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.T.Map.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.T.Map.cs
@@ -1,9 +1,13 @@
 ﻿#nullable enable
 
+using System.Threading.Tasks;
+
 namespace System
 {
     partial struct Result<TSuccess, TFailure>
     {
+        // Map
+
         public Result<TResultSuccess, TFailure> MapSuccess<TResultSuccess>(
             Func<TSuccess, TResultSuccess> mapSuccess)
             where TResultSuccess : notnull
@@ -23,6 +27,50 @@ namespace System
             where TResultFailure : notnull, new()
             =>
             MapSource.Map(mapSuccess, mapFailure).AsResult();
+
+        // Map Async / Task
+
+        public async Task<Result<TResultSuccess, TFailure>> MapSuccessAsync<TResultSuccess>(
+            Func<TSuccess, Task<TResultSuccess>> mapSuccessAsync)
+            where TResultSuccess : notnull
+            =>
+            (await MapSuccessSource.MapFirstAsync(mapSuccessAsync).ConfigureAwait(false)).AsResult();
+
+        public async Task<Result<TSuccess, TResultFailure>> MapFailureAsync<TResultFailure>(
+            Func<TFailure, Task<TResultFailure>> mapFailureAsync)
+            where TResultFailure : notnull, new()
+            =>
+            (await MapFailureSource.MapSecondAsync(mapFailureAsync).ConfigureAwait(false)).AsResult();
+
+        public async Task<Result<TResultSuccess, TResultFailure>> MapAsync<TResultSuccess, TResultFailure>(
+            Func<TSuccess, Task<TResultSuccess>> mapSuccessAsync,
+            Func<TFailure, Task<TResultFailure>> mapFailureAsync)
+            where TResultSuccess : notnull
+            where TResultFailure : notnull, new()
+            =>
+            (await MapSource.MapAsync(mapSuccessAsync, mapFailureAsync).ConfigureAwait(false)).AsResult();
+
+        // Map Async / ValueTask
+
+        public async ValueTask<Result<TResultSuccess, TFailure>> MapSuccessValueAsync<TResultSuccess>(
+            Func<TSuccess, ValueTask<TResultSuccess>> mapSuccessAsync)
+            where TResultSuccess : notnull
+            =>
+            (await MapSuccessSource.MapFirstValueAsync(mapSuccessAsync).ConfigureAwait(false)).AsResult();
+
+        public async ValueTask<Result<TSuccess, TResultFailure>> MapFailureValueAsync<TResultFailure>(
+            Func<TFailure, ValueTask<TResultFailure>> mapFailureAsync)
+            where TResultFailure : notnull, new()
+            =>
+            (await MapFailureSource.MapSecondValueAsync(mapFailureAsync).ConfigureAwait(false)).AsResult();
+
+        public async ValueTask<Result<TResultSuccess, TResultFailure>> MapValueAsync<TResultSuccess, TResultFailure>(
+            Func<TSuccess, ValueTask<TResultSuccess>> mapSuccessAsync,
+            Func<TFailure, ValueTask<TResultFailure>> mapFailureAsync)
+            where TResultSuccess : notnull
+            where TResultFailure : notnull, new()
+            =>
+            (await MapSource.MapValueAsync(mapSuccessAsync, mapFailureAsync).ConfigureAwait(false)).AsResult();
 
         private TaggedUnion<TSuccess, TFailure> MapSuccessSource => union;
 
