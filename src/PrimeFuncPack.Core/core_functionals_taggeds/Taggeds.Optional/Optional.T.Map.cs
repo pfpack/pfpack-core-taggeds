39b6fa5471cc0494610972690730aa6cec1421d3
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-09-10 21:16:47 +0300

    Optional: Add Filter/Map async

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Optional/Optional.T.Map.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Optional/Optional.T.Map.cs
@@ -1,5 +1,7 @@
 ﻿#nullable enable
 
+using System.Threading.Tasks;
+
 namespace System
 {
     partial struct Optional<T>
@@ -8,9 +10,33 @@ namespace System
         {
             _ = map ?? throw new ArgumentNullException(nameof(map));
 
-            return ImplFold(Map, () => default);
+            return ImplFold(MapPresent, () => default);
 
-            Optional<TResult> Map(T value) => Optional<TResult>.Present(map.Invoke(value));
+            Optional<TResult> MapPresent(T value)
+                =>
+                Optional<TResult>.Present(map.Invoke(value));
+        }
+
+        public Task<Optional<TResult>> MapAsync<TResult>(Func<T, Task<TResult>> mapAsync)
+        {
+            _ = mapAsync ?? throw new ArgumentNullException(nameof(mapAsync));
+
+            return ImplFold(MapPresentAsync, () => default(Optional<TResult>));
+
+            async Task<Optional<TResult>> MapPresentAsync(T value)
+                =>
+                Optional<TResult>.Present(await mapAsync.Invoke(value).ConfigureAwait(false));
+        }
+
+        public ValueTask<Optional<TResult>> MapValueAsync<TResult>(Func<T, ValueTask<TResult>> mapAsync)
+        {
+            _ = mapAsync ?? throw new ArgumentNullException(nameof(mapAsync));
+
+            return ImplFold(MapPresentValueAsync, () => default(Optional<TResult>));
+
+            async ValueTask<Optional<TResult>> MapPresentValueAsync(T value)
+                =>
+                Optional<TResult>.Present(await mapAsync.Invoke(value).ConfigureAwait(false));
         }
     }
 }
