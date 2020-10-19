b77097a1d429e80e07062870f8f0017e7b96ef4c
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-10-20 00:05:12 +0300

    Linq: Add ForEach IReadOnlyList based

---
+++ src/PrimeFuncPack.Linq/Linq.Collections/Collections/CollectionsExtensions.ForEach.IReadOnlyList.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    partial class CollectionsExtensions
+    {
+        public static void ForEach<TSource>(
+            this IReadOnlyList<TSource> source,
+            Action<TSource> action)
+        {
+            _ = source ?? throw new ArgumentNullException(nameof(source));
+            _ = action ?? throw new ArgumentNullException(nameof(action));
+
+            source.InternalForEach(action);
+        }
+
+        public static void ForEach<TSource>(
+            this IReadOnlyList<TSource> source,
+            Action<int, TSource> action)
+        {
+            _ = source ?? throw new ArgumentNullException(nameof(source));
+            _ = action ?? throw new ArgumentNullException(nameof(action));
+
+            source.InternalForEach(action);
+        }
+    }
+}
