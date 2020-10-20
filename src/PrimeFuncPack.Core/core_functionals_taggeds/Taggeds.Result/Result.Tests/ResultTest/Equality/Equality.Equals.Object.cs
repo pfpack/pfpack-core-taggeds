0c6b055af0d3c3829c0a93dc0124b091a1a03635
pmosk <p@moskovoi.ru>
2020-10-21 02:38:36 +0300

    Add Result.Equality unit tests

---
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Tests/ResultTest/Equality/Equality.Equals.Object.cs
@@ -0,0 +1,251 @@
+﻿#nullable enable
+
+using NUnit.Framework;
+using PrimeFuncPack.UnitTest;
+using System;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Functionals.Taggeds.Tests
+{
+    partial class ResultTest
+    {
+        [Test]
+        public void EqualsObject_SourceIsDefaultAndObjectIsDefaultResultSameType_ExpectTrue()
+        {
+            var source = default(Result<RefType, StructType>);
+            object? obj = new Result<RefType, StructType>();
+
+            var actual = source.Equals(obj);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsSuccessAndObjectIsSuccessAndValuesAreEqual_ExpectTrue()
+        {
+            var sourceValue = PlusFifteenIdRefType;
+            var source = Result<StructType, RefType>.Failure(sourceValue);
+
+            var objValue = sourceValue;
+            object? obj = Result<StructType, RefType>.Failure(objValue);
+
+            var actual = source.Equals(obj);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsFailureAndObjectIsFailureAndValuesAreEqual_ExpectTrue()
+        {
+            var text = "Some new text.";
+            var sourceValue = new StructType
+            {
+                Text = text
+            };
+            var source = Result<RefType, StructType>.Failure(sourceValue);
+
+            var objValue = new StructType
+            {
+                Text = text
+            };
+            object? obj = Result<RefType, StructType>.Failure(objValue);
+
+            var actual = source.Equals(obj);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsFailureWithDefaultValueAndObjectIsDefault_ExpectTrue()
+        {
+            var source = Result<RefType, StructType>.Failure(default);
+            object? obj = default(Result<RefType, StructType>);
+
+            var actual = source.Equals(obj);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsDefaultAndObjectIsFailureWithDefaultValue_ExpectTrue()
+        {
+            var source = default(Result<RefType, DateTime>);
+            object? obj = Result<RefType, DateTime>.Failure(default);
+            
+            var actual = source.Equals(obj);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsSuccessAndObjectIsSuccessAndValuesAreNotEqual_ExpectFalse()
+        {
+            var sourceValue = new RefType();
+            var source = Result<RefType, StructType>.Success(sourceValue);
+
+            var objValue = new RefType();
+            object? obj = Result<RefType, StructType>.Success(objValue);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsFailureAndObjectIsFailureAndValuesAreNotEqual_ExpectFalse()
+        {
+            var text = "Some new text.";
+            var sourceValue = new StructType
+            {
+                Text = text
+            };
+            var source = Result<RefType, StructType>.Failure(sourceValue);
+
+            var objValue = new StructType
+            {
+                Text = text + ".."
+            };
+            object? obj = Result<RefType, StructType>.Failure(objValue);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsSuccessAndObjectIsFailure_ExpectFalse()
+        {
+            var sourceValue = MinusFifteenIdRefType;
+
+            var source = Result<RefType, RefType>.Success(sourceValue);
+            object? obj = Result<RefType, RefType>.Failure(sourceValue);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsSuccessAndObjectIsDefault_ExpectFalse()
+        {
+            var source = Result<StructType, RefType>.Success(default);
+            object? obj = default(Result<StructType, RefType>);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsFailureAndObjectIsSuccess_ExpectFalse()
+        {
+            var sourceValue = SomeTextStructType;
+
+            var source = Result<StructType, StructType>.Failure(sourceValue);
+            object? obj = Result<StructType, StructType>.Success(sourceValue);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsFailureWithNotDefaultValueAndObjectIsDefault_ExpectFalse()
+        {
+            var source = Result<RefType, StructType>.Failure(SomeTextStructType);
+            object? obj = default(Result<RefType, StructType>);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsDefaultAndObjectIsSuccess_ExpectFalse()
+        {
+            var source = default(Result<int, RefType>);
+            object? obj = Result<int, RefType>.Success(default);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsDefaultAndObjectIsFailureWithNotDefaultValue_ExpectFalse()
+        {
+            var source = default(Result<StructType, RefType>);
+            object? obj = Result<StructType, RefType>.Failure(ZeroIdRefType);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TestDataSource), nameof(TestDataSource.ResultTestSource))]
+        public void EqualsObject_ObjectIsNull_ExpectFalse(
+            in Result<RefType, StructType> source)
+        {
+            object? obj = null;
+            var actual = source.Equals(obj);
+
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsDefaultAndObjectIsNotResult_ExpectFalse()
+        {
+            var source = default(Result<RefType, StructType>);
+            object? obj = new StructType();
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsDefaultAndObjectIsOtherTypeDefaultResult_ExpectFalse()
+        {
+            var source = default(Result<RefType, decimal>);
+            object? obj = default(Result<RefType, int>);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsSuccessAndObjectIsNotResult_ExpectFalse()
+        {
+            var sourceValue = SomeTextStructType;
+
+            var source = Result<StructType, RefType>.Success(sourceValue);
+            object? obj = sourceValue;
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsSuccessAndObjectIsOtherTypeSuccessResult_ExpectFalse()
+        {
+            var sourceValue = PlusFifteenIdRefType;
+
+            var source = Result<RefType, StructType>.Success(sourceValue);
+            object? obj = Result<RefType, int>.Success(sourceValue);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsFailureAndObjectIsNotResult_ExpectFalse()
+        {
+            var sourceValue = ZeroIdRefType;
+
+            var source = Result<StructType, RefType>.Failure(sourceValue);
+            object? obj = sourceValue;
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsObject_SourceIsFailureAndObjectIsOtherTypeFailureResult_ExpectFalse()
+        {
+            var sourceValue = SomeTextStructType;
+
+            var source = Result<int, StructType>.Failure(sourceValue);
+            object? obj = Result<uint, StructType>.Failure(sourceValue);
+
+            var actual = source.Equals(obj);
+            Assert.False(actual);
+        }
+    }
+}
---
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result.Tests/ResultTest/Equality/Equality.Equals.Other.cs
@@ -0,0 +1,172 @@
+﻿#nullable enable
+
+using NUnit.Framework;
+using PrimeFuncPack.UnitTest;
+using System;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Functionals.Taggeds.Tests
+{
+    partial class ResultTest
+    {
+        [Test]
+        public void EqualsOther_SourceIsDefaultAndOtherIsDefault_ExpectTrue()
+        {
+            var source = default(Result<RefType, StructType>);
+            var other = new Result<RefType, StructType>();
+
+            var actual = source.Equals(other);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsSuccessAndOtherIsSuccessAndValuesAreEqual_ExpectTrue()
+        {
+            var text = "Some new text.";
+            var sourceValue = new StructType
+            {
+                Text = text
+            };
+            var source = Result<StructType, object>.Success(sourceValue);
+
+            var otherValue = new StructType
+            {
+                Text = text
+            };
+            Result<StructType, object> other = otherValue;
+
+            var actual = source.Equals(other);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsFailureAndOtherIsFailureAndValuesAreEqual_ExpectTrue()
+        {
+            var sourceValue = new object();
+            Result<RefType, object> source = sourceValue;
+
+            var otherValue = sourceValue;
+            var other = Result<RefType, object>.Failure(otherValue);
+
+            var actual = source.Equals(other);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsFailureWithDefaultValueAndOtherIsDefault_ExpectTrue()
+        {
+            var source = Result<RefType, StructType>.Failure(default);
+            var other = default(Result<RefType, StructType>);
+
+            var actual = source.Equals(other);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsDefaultAndOtherIsFailureWithDefaultValue_ExpectTrue()
+        {
+            var source = default(Result<RefType, DateTime>);
+            Result<RefType, DateTime> other = default(DateTime);
+
+            var actual = source.Equals(other);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsSuccessAndOtherIsSuccessAndValuesAreNotEqual_ExpectFalse()
+        {
+            var sourceValue = new object();
+            var source = Result<object, StructType>.Success(sourceValue);
+
+            var otherValue = new object();
+            var other = Result<object, StructType>.Success(otherValue);
+
+            var actual = source.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsFailureAndOtherIsFailureAndValuesAreNotEqual_ExpectFalse()
+        {
+            var text = "Some new text.";
+            var sourceValue = new StructType
+            {
+                Text = text
+            };
+            var source = Result<RefType, StructType>.Failure(sourceValue);
+
+            var otherValue = new StructType
+            {
+                Text = text + ".."
+            };
+            var other = Result<RefType, StructType>.Failure(otherValue);
+
+            var actual = source.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsSuccessAndOtherIsFailure_ExpectFalse()
+        {
+            var sourceValue = SomeTextStructType;
+
+            var source = Result<StructType, StructType>.Success(sourceValue);
+            var other = Result<StructType, StructType>.Failure(sourceValue);
+
+            var actual = source.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsSuccessAndOtherIsDefault_ExpectFalse()
+        {
+            var source = Result<RefType, StructType>.Success(ZeroIdRefType);
+            var other = default(Result<RefType, StructType>);
+
+            var actual = source.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsFailureAndOtherIsSuccess_ExpectFalse()
+        {
+            var sourceValue = PlusFifteenIdRefType;
+
+            var source = Result<RefType, RefType>.Failure(sourceValue);
+            var other = Result<RefType, RefType>.Success(sourceValue);
+
+            var actual = source.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsFailureWithNotDefaultValueAndOtherIsDefault_ExpectFalse()
+        {
+            var source = Result<RefType, StructType>.Failure(SomeTextStructType);
+            var other = default(Result<RefType, StructType>);
+
+            var actual = source.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsDefaultAndOtherIsSuccess_ExpectFalse()
+        {
+            var source = default(Result<StructType, RefType>);
+            var other = Result<StructType, RefType>.Success(default);
+
+            var actual = source.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsOther_SourceIsDefaultAndOtherIsFailureWithNotDefaultValue_ExpectFalse()
+        {
+            var source = default(Result<StructType, RefType>);
+            var other = Result<StructType, RefType>.Failure(new RefType());
+
+            var actual = source.Equals(other);
+            Assert.False(actual);
+        }
+    }
+}
