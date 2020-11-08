1e3c388aeaca0b91445485be86d320040b62bbfa
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-11-08 23:30:13 +0300

    Implement ForwardCatching

---
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result/Result.T.Flow.ForwardCatching.Internal.cs
@@ -0,0 +1,60 @@
+﻿#nullable enable
+
+using System.Threading.Tasks;
+
+namespace System
+{
+    partial struct Result<TSuccess, TFailure>
+    {
+        private static Result<TNextSuccess, TNextFailure> InternalNextFactoryCatching<TNextSuccess, TNextFailure>(
+            TSuccess success,
+            Func<TSuccess, Result<TNextSuccess, TNextFailure>> nextFactory,
+            Func<Exception, TNextFailure> failureFactory)
+            where TNextSuccess : notnull
+            where TNextFailure : struct
+        {
+            try
+            {
+                return nextFactory.Invoke(success);
+            }
+            catch (Exception ex)
+            {
+                return failureFactory.Invoke(ex);
+            }
+        }
+
+        private static Task<Result<TNextSuccess, TNextFailure>> InternalNextFactoryCatchingAsync<TNextSuccess, TNextFailure>(
+            TSuccess success,
+            Func<TSuccess, Task<Result<TNextSuccess, TNextFailure>>> nextFactoryAsync,
+            Func<Exception, TNextFailure> failureFactory)
+            where TNextSuccess : notnull
+            where TNextFailure : struct
+        {
+            try
+            {
+                return nextFactoryAsync.Invoke(success);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult<Result<TNextSuccess, TNextFailure>>(failureFactory.Invoke(ex));
+            }
+        }
+
+        private static ValueTask<Result<TNextSuccess, TNextFailure>> InternalNextFactoryCatchingValueAsync<TNextSuccess, TNextFailure>(
+            TSuccess success,
+            Func<TSuccess, ValueTask<Result<TNextSuccess, TNextFailure>>> nextFactoryAsync,
+            Func<Exception, TNextFailure> failureFactory)
+            where TNextSuccess : notnull
+            where TNextFailure : struct
+        {
+            try
+            {
+                return nextFactoryAsync.Invoke(success);
+            }
+            catch (Exception ex)
+            {
+                return ValueTask.FromResult<Result<TNextSuccess, TNextFailure>>(failureFactory.Invoke(ex));
+            }
+        }
+    }
+}
---
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.Result/Result/Result.T.Flow.ForwardCatching.cs
@@ -0,0 +1,144 @@
+﻿#nullable enable
+
+using System.Threading.Tasks;
+
+namespace System
+{
+    partial struct Result<TSuccess, TFailure>
+    {
+        // ForwardCatching
+
+        public Result<TNextSuccess, TNextFailure> ForwardCatching<TNextSuccess, TNextFailure>(
+            Func<TSuccess, Result<TNextSuccess, TNextFailure>> nextFactory,
+            Func<TFailure, TNextFailure> mapFailure,
+            Func<Exception, TNextFailure> failureFactory)
+            where TNextSuccess : notnull
+            where TNextFailure : struct
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+            _ = mapFailure ?? throw new ArgumentNullException(nameof(mapFailure));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            return Fold(
+                success => InternalNextFactoryCatching(success, nextFactory, failureFactory),
+                failure => mapFailure.Invoke(failure));
+        }
+
+        public Result<TNextSuccess, TFailure> ForwardCatching<TNextSuccess>(
+            Func<TSuccess, Result<TNextSuccess, TFailure>> nextFactory,
+            Func<Exception, TFailure> failureFactory)
+            where TNextSuccess : notnull
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            return Fold(
+                success => InternalNextFactoryCatching(success, nextFactory, failureFactory),
+                failure => failure);
+        }
+
+        public Result<TSuccess, TFailure> ForwardCatching(
+            Func<TSuccess, Result<TSuccess, TFailure>> nextFactory,
+            Func<Exception, TFailure> failureFactory)
+        {
+            _ = nextFactory ?? throw new ArgumentNullException(nameof(nextFactory));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            var @this = this;
+
+            return Fold(
+                success => InternalNextFactoryCatching(success, nextFactory, failureFactory),
+                _ => @this);
+        }
+
+        // ForwardCatching Async / Task
+
+        public Task<Result<TNextSuccess, TNextFailure>> ForwardCatchingAsync<TNextSuccess, TNextFailure>(
+            Func<TSuccess, Task<Result<TNextSuccess, TNextFailure>>> nextFactoryAsync,
+            Func<TFailure, TNextFailure> mapFailure,
+            Func<Exception, TNextFailure> failureFactory)
+            where TNextSuccess : notnull
+            where TNextFailure : struct
+        {
+            _ = nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync));
+            _ = mapFailure ?? throw new ArgumentNullException(nameof(mapFailure));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            return FoldAsync(
+                success => InternalNextFactoryCatchingAsync(success, nextFactoryAsync, failureFactory),
+                failure => Task.FromResult<Result<TNextSuccess, TNextFailure>>(mapFailure.Invoke(failure)));
+        }
+
+        public Task<Result<TNextSuccess, TFailure>> ForwardCatchingAsync<TNextSuccess>(
+            Func<TSuccess, Task<Result<TNextSuccess, TFailure>>> nextFactoryAsync,
+            Func<Exception, TFailure> failureFactory)
+            where TNextSuccess : notnull
+        {
+            _ = nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            return FoldAsync(
+                success => InternalNextFactoryCatchingAsync(success, nextFactoryAsync, failureFactory),
+                failure => Task.FromResult<Result<TNextSuccess, TFailure>>(failure));
+        }
+
+        public Task<Result<TSuccess, TFailure>> ForwardCatchingAsync(
+            Func<TSuccess, Task<Result<TSuccess, TFailure>>> nextFactoryAsync,
+            Func<Exception, TFailure> failureFactory)
+        {
+            _ = nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            var @this = this;
+
+            return FoldAsync(
+                success => InternalNextFactoryCatchingAsync(success, nextFactoryAsync, failureFactory),
+                _ => Task.FromResult(@this));
+        }
+
+        // ForwardCatching Async / ValueTask
+
+        public ValueTask<Result<TNextSuccess, TNextFailure>> ForwardCatchingValueAsync<TNextSuccess, TNextFailure>(
+            Func<TSuccess, ValueTask<Result<TNextSuccess, TNextFailure>>> nextFactoryAsync,
+            Func<TFailure, TNextFailure> mapFailure,
+            Func<Exception, TNextFailure> failureFactory)
+            where TNextSuccess : notnull
+            where TNextFailure : struct
+        {
+            _ = nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync));
+            _ = mapFailure ?? throw new ArgumentNullException(nameof(mapFailure));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            return FoldValueAsync(
+                success => InternalNextFactoryCatchingValueAsync(success, nextFactoryAsync, failureFactory),
+                failure => ValueTask.FromResult<Result<TNextSuccess, TNextFailure>>(mapFailure.Invoke(failure)));
+        }
+
+        public ValueTask<Result<TNextSuccess, TFailure>> ForwardCatchingValueAsync<TNextSuccess>(
+            Func<TSuccess, ValueTask<Result<TNextSuccess, TFailure>>> nextFactoryAsync,
+            Func<Exception, TFailure> failureFactory)
+            where TNextSuccess : notnull
+        {
+            _ = nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            return FoldValueAsync(
+                success => InternalNextFactoryCatchingValueAsync(success, nextFactoryAsync, failureFactory),
+                failure => ValueTask.FromResult<Result<TNextSuccess, TFailure>>(failure));
+        }
+
+        public ValueTask<Result<TSuccess, TFailure>> ForwardCatchingValueAsync(
+            Func<TSuccess, ValueTask<Result<TSuccess, TFailure>>> nextFactoryAsync,
+            Func<Exception, TFailure> failureFactory)
+        {
+            _ = nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync));
+            _ = failureFactory ?? throw new ArgumentNullException(nameof(failureFactory));
+
+            var @this = this;
+
+            return FoldValueAsync(
+                success => InternalNextFactoryCatchingValueAsync(success, nextFactoryAsync, failureFactory),
+                _ => ValueTask.FromResult(@this));
+        }
+    }
+}
