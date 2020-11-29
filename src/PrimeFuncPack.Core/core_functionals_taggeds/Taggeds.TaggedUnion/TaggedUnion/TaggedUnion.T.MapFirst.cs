2767549f3db0efc6b58fd099aeef2472f34926ac
Andrei Sergeev <a.sergeev.official@gmail.com>
2020-11-29 13:46:17 +0300

    TaggedUnion: Adjust MapFirst/Second

--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.TaggedUnion/TaggedUnion/TaggedUnion.T.MapFirst.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.TaggedUnion/TaggedUnion/TaggedUnion.T.MapFirst.cs
@@ -8,17 +8,35 @@ namespace System
     {
         public TaggedUnion<TResultFirst, TSecond> MapFirst<TResultFirst>(
             Func<TFirst, TResultFirst> mapFirst)
-            =>
-            Map(mapFirst, Pipeline.Pipe);
+        {
+            _ = mapFirst ?? throw new ArgumentNullException(nameof(mapFirst));
+
+            return Fold<TaggedUnion<TResultFirst, TSecond>>(
+                value => mapFirst.Invoke(value),
+                static value => value,
+                static () => default);
+        }
 
         public Task<TaggedUnion<TResultFirst, TSecond>> MapFirstAsync<TResultFirst>(
             Func<TFirst, Task<TResultFirst>> mapFirstAsync)
-            =>
-            MapAsync(mapFirstAsync, Task.FromResult);
+        {
+            _ = mapFirstAsync ?? throw new ArgumentNullException(nameof(mapFirstAsync));
+
+            return FoldAsync<TaggedUnion<TResultFirst, TSecond>>(
+                async value => await mapFirstAsync.Invoke(value).ConfigureAwait(false),
+                static value => Task.FromResult<TaggedUnion<TResultFirst, TSecond>>(value),
+                static () => Task.FromResult<TaggedUnion<TResultFirst, TSecond>>(default));
+        }
 
         public ValueTask<TaggedUnion<TResultFirst, TSecond>> MapFirstValueAsync<TResultFirst>(
             Func<TFirst, ValueTask<TResultFirst>> mapFirstAsync)
-            =>
-            MapValueAsync(mapFirstAsync, ValueTask.FromResult);
+        {
+            _ = mapFirstAsync ?? throw new ArgumentNullException(nameof(mapFirstAsync));
+
+            return FoldValueAsync<TaggedUnion<TResultFirst, TSecond>>(
+                async value => await mapFirstAsync.Invoke(value).ConfigureAwait(false),
+                static value => ValueTask.FromResult<TaggedUnion<TResultFirst, TSecond>>(value),
+                static () => default(ValueTask<TaggedUnion<TResultFirst, TSecond>>));
+        }
     }
 }
--- src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.TaggedUnion/TaggedUnion/TaggedUnion.T.MapSecond.cs
+++ src/PrimeFuncPack.Core/core_functionals_taggeds/Taggeds.TaggedUnion/TaggedUnion/TaggedUnion.T.MapSecond.cs
@@ -8,17 +8,35 @@ namespace System
     {
         public TaggedUnion<TFirst, TResultSecond> MapSecond<TResultSecond>(
             Func<TSecond, TResultSecond> mapSecond)
-            =>
-            Map(Pipeline.Pipe, mapSecond);
+        {
+            _ = mapSecond ?? throw new ArgumentNullException(nameof(mapSecond));
+
+            return Fold<TaggedUnion<TFirst, TResultSecond>>(
+                static value => value,
+                value => mapSecond.Invoke(value),
+                static () => default);
+        }
 
         public Task<TaggedUnion<TFirst, TResultSecond>> MapSecondAsync<TResultSecond>(
             Func<TSecond, Task<TResultSecond>> mapSecondAsync)
-            =>
-            MapAsync(Task.FromResult, mapSecondAsync);
+        {
+            _ = mapSecondAsync ?? throw new ArgumentNullException(nameof(mapSecondAsync));
+
+            return FoldAsync<TaggedUnion<TFirst, TResultSecond>>(
+                static value => Task.FromResult<TaggedUnion<TFirst, TResultSecond>>(value),
+                async value => await mapSecondAsync.Invoke(value).ConfigureAwait(false),
+                static () => Task.FromResult<TaggedUnion<TFirst, TResultSecond>>(default));
+        }
 
         public ValueTask<TaggedUnion<TFirst, TResultSecond>> MapSecondValueAsync<TResultSecond>(
             Func<TSecond, ValueTask<TResultSecond>> mapSecondAsync)
-            =>
-            MapValueAsync(ValueTask.FromResult, mapSecondAsync);
+        {
+            _ = mapSecondAsync ?? throw new ArgumentNullException(nameof(mapSecondAsync));
+
+            return FoldValueAsync<TaggedUnion<TFirst, TResultSecond>>(
+                static value => ValueTask.FromResult<TaggedUnion<TFirst, TResultSecond>>(value),
+                async value => await mapSecondAsync.Invoke(value).ConfigureAwait(false),
+                static () => default(ValueTask<TaggedUnion<TFirst, TResultSecond>>));
+        }
     }
 }
