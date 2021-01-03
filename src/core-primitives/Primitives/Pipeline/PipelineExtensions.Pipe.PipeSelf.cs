76fff61efca09581c7d9d7fddf69750e96791ff6
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-01-04 00:13:04 +0300

    Refactor: Join Pipe / PipeSelf files

--- src/core-primitives/Primitives/Pipeline/PipelineExtensions.Pipe.PipeSelf.cs
+++ src/core-primitives/Primitives/Pipeline/PipelineExtensions.Pipe.PipeSelf.cs
@@ -1,9 +1,20 @@
 ﻿#nullable enable
 
-using System;
+namespace System
+{
+    partial class PipelineExtensions
+    {
+        public static TResult Pipe<T, TResult>(this T value, Func<T, TResult> pipe)
+            =>
+            (pipe ?? throw new ArgumentNullException(nameof(pipe)))
+            .Invoke(value);
+    }
+}
 
 namespace PrimeFuncPack
 {
+    using System;
+
     partial class PipelineExtensions
     {
         public static TResult PipeSelf<T, TResult>(this T value, Func<T, TResult> pipe)
