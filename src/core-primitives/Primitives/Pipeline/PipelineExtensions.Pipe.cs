96371764bdc1bb900109205899af52518a646c25
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-01-03 21:47:21 +0300

    Add PipelineExtensions (TODO: Add tests)

---
+++ src/core-primitives/Primitives/Pipeline/PipelineExtensions.Pipe.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+namespace System
+{
+    partial class PipelineExtensions
+    {
+        public static TResult Pipe<T, TResult>(this T value, Func<T, TResult> pipe)
+            =>
+            (pipe ?? throw new ArgumentNullException(nameof(pipe)))
+            .Invoke(value);
+    }
+}
