1385e66d61a70eb8aa6612b3f1b38e50144488a2
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-01-06 20:59:16 +0300

    Optional: Add InternalHandle; Unit: Add Get factory, add tests

---
+++ src/core-taggeds-optional/Optional/Optional.T.InternalOn.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+namespace System
+{
+    partial struct Optional<T>
+    {
+        private TResult InternalOn<TResult>(
+            Func<T, Unit> onPresent,
+            Func<Unit> onElse,
+            Func<TResult> resultSupplier)
+        {
+            _ = InternalHandle(Value, onPresent, onElse);
+            return resultSupplier.Invoke();
+        }
+
+        private TResult InternalOnPresent<TResult>(
+            Func<T, Unit> handler,
+            Func<TResult> resultSupplier)
+        {
+            _ = InternalHandle(Value, handler, Unit.Get);
+            return resultSupplier.Invoke();
+        }
+
+        private TResult InternalOnAbsent<TResult>(
+            Func<Unit> handler,
+            Func<TResult> resultSupplier)
+        {
+            _ = InternalHandle(Unit.Get, Pipeline.Pipe, handler);
+            return resultSupplier.Invoke();
+        }
+    }
+}
