e8222c02fc65d6d494d2a1654fdaaabe70e4d766
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-02-07 12:26:07 +0300

    Orginize Unit extensions tests; Add tests for obsoletes

---
+++ src/core-unit/Unit.Tests/UnitExtensionsObsoleteTests/Obsolete.ExtensionsClass_ExpectIsObsolete.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace PrimeFuncPack.Core.Tests
+{
+    partial class UnitExtensionsObsoleteTests
+    {
+        [Test]
+        [TestCase("System.InvokeThenToUnitActionExtensions")]
+        [TestCase("System.InvokeThenToUnitFuncAsyncExtensions")]
+        [TestCase("System.InvokeThenToUnitValueAsyncExtensions")]
+        public void ExtensionsClass_ExpectIsObsolete(string fullName)
+        {
+            var type = typeof(UnitExtensions).Assembly.GetType(
+                fullName,
+                throwOnError: false,
+                ignoreCase: false);
+
+            Assert.IsNotNull(type);
+            Assert.IsTrue(type!.Attributes.HasFlag(TypeAttributes.Public));
+            Assert.IsTrue(type!.Attributes.HasFlag(TypeAttributes.Sealed));
+            Assert.IsEmpty(type.GetConstructors());
+
+            Assert.IsTrue(
+                type.CustomAttributes.Any(
+                    attr
+                    =>
+                    attr.AttributeType == typeof(ObsoleteAttribute) &&
+                    attr.ConstructorArguments.Count == 2 &&
+                    attr.ConstructorArguments[0].ArgumentType == typeof(string) &&
+                    attr.ConstructorArguments[1].ArgumentType == typeof(bool) &&
+                    attr.ConstructorArguments[1].Value is true));
+        }
+    }
+}
---
+++ src/core-unit/Unit.Tests/UnitObsoleteTests/Tests.cs
@@ -0,0 +1,8 @@
+﻿#nullable enable
+
+namespace PrimeFuncPack.Core.Tests
+{
+    public sealed partial class UnitObsoleteTests
+    {
+    }
+}
