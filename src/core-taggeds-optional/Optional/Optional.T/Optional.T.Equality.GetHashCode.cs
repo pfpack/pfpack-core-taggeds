79a4102aba695cfeed45447f37e19be38536dfb8
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-12 01:55:00 +0300

    Optional: Refactor GetHashCode: Use ternary op

--- src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.GetHashCode.cs
+++ src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.GetHashCode.cs
@@ -5,15 +5,11 @@ namespace System
     partial struct Optional<T>
     {
         public override int GetHashCode()
-        {
-            if (hasValue)
-            {
-                return value is not null
+            =>
+            hasValue
+                ? value is not null
                     ? HashCode.Combine(EqualityContract, true, EqualityComparer.GetHashCode(value))
-                    : HashCode.Combine(EqualityContract, true);
-            }
-
-            return HashCode.Combine(EqualityContract, false);
-        }
+                    : HashCode.Combine(EqualityContract, true)
+                : HashCode.Combine(EqualityContract, false);
     }
 }
