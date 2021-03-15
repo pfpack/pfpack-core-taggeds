a989e824ec80f428408e325731feadbc2efd5843
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-15 10:43:13 +0300

    Refactor

--- src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/FilterNotNullOrThrow/PredicateFactories.cs
+++ src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/FilterNotNullOrThrow/PredicateFactories.cs
@@ -9,12 +9,15 @@ namespace System
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Func<T, bool> CreateFilterNotNullOrThrowPredicate<T>()
             =>
-            value => value is not null
-                ? true
-                : throw CreateExpectedNotNullOrAbsentException();
+            ImplCreateFilterNotNullOrThrowPredicate<T>(CreateExpectedNotNullOrAbsentException);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Func<T, bool> CreateFilterNotNullOrThrowPredicate<T>(Func<Exception> exceptionFactory)
+            =>
+            ImplCreateFilterNotNullOrThrowPredicate<T>(exceptionFactory);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Func<T, bool> ImplCreateFilterNotNullOrThrowPredicate<T>(Func<Exception> exceptionFactory)
             =>
             value => value is not null
                 ? true
