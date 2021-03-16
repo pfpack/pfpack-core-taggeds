4abf91326d2ea0825109bf8f9802d6e770192749
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-17 01:28:43 +0300

    Implement the new FilterNotNullOrThrow proto

--- src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/FilterNotNullOrThrow/FilterNotNullOrThrow.cs
+++ src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/FilterNotNullOrThrow/FilterNotNullOrThrow.cs
@@ -4,20 +4,36 @@ namespace System
 {
     partial class FilterNotNullOptionalExtensions
     {
-        public static Optional<T> FilterNotNullOrThrow<T>(
-            this Optional<T> optional)
+        public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional)
             =>
             optional
-            .InternalFilterNotNullOrThrow();
+            .Filter(value => value is not null ? true : throw CreateExpectedNotNullOrAbsentException())
+            .Map(value => value ?? throw CreateUnexpectedNullException_MustNeverBeInvoked());
 
-        public static Optional<T> FilterNotNullOrThrow<T>(
-            this Optional<T> optional,
-            Func<Exception> exceptionFactory)
+        public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional, Func<Exception> exceptionFactory)
         {
             _ = exceptionFactory ?? throw new ArgumentNullException(nameof(exceptionFactory));
 
             return optional
-                .InternalFilterNotNullOrThrow(exceptionFactory);
+            .Filter(value => value is not null ? true : throw exceptionFactory.Invoke())
+            .Map(value => value ?? throw CreateUnexpectedNullException_MustNeverBeInvoked());
+        }
+
+        public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional)
+            where T : struct
+            =>
+            optional
+            .Filter(value => value is not null ? true : throw CreateExpectedNotNullOrAbsentException())
+            .Map(value => value ?? throw CreateUnexpectedNullException_MustNeverBeInvoked());
+
+        public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional, Func<Exception> exceptionFactory)
+            where T : struct
+        {
+            _ = exceptionFactory ?? throw new ArgumentNullException(nameof(exceptionFactory));
+
+            return optional
+            .Filter(value => value is not null ? true : throw exceptionFactory.Invoke())
+            .Map(value => value ?? throw CreateUnexpectedNullException_MustNeverBeInvoked());
         }
     }
 }
