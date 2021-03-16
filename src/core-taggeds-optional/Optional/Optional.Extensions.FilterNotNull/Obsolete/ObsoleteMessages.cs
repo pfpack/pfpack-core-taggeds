7ab466c5d24e602f72e154e9ee6611060faf9dc3
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-17 01:16:45 +0300

    Deprecate FilterNotNullOrThrowThenMap

--- src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/Obsolete/ObsoleteMessages.cs
+++ src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/Obsolete/ObsoleteMessages.cs
@@ -7,6 +7,8 @@ namespace System
         private static class ObsoleteMessages
         {
             public const string FilterNotNullThenMap = "This method is obsolete. Call FilterNotNull instead.";
+
+            public const string FilterNotNullOrThrowThenMap = "This method is obsolete. Call FilterNotNullOrThrow instead.";
         }
     }
 }
