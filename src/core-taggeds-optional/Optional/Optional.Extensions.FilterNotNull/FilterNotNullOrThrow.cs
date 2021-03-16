95395f2066435959baf6f0af25a90647374de436
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-17 02:54:54 +0300

    Refactor FilterNotNullOrThrow: implement the internals

--- src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/FilterNotNullOrThrow.cs
+++ src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/FilterNotNullOrThrow.cs
@@ -6,34 +6,26 @@ namespace System
     {
         public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional)
             =>
-            optional
-            .Filter(InternalCreateFilterNotNullOrThrowPredicate_Common<T>(CreateExpectedNotNullOrAbsentException))
-            .Map(InternalMapToNonNullable);
+            optional.InternalFilterNotNullOrThrow(CreateExpectedNotNullOrAbsentException);
 
         public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional, Func<Exception> exceptionFactory)
         {
             _ = exceptionFactory ?? throw new ArgumentNullException(nameof(exceptionFactory));
 
-            return optional
-            .Filter(InternalCreateFilterNotNullOrThrowPredicate_Common<T>(exceptionFactory))
-            .Map(InternalMapToNonNullable);
+            return optional.InternalFilterNotNullOrThrow(exceptionFactory);
         }
 
         public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional)
             where T : struct
             =>
-            optional
-            .Filter(InternalCreateFilterNotNullOrThrowPredicate_Struct<T>(CreateExpectedNotNullOrAbsentException))
-            .Map(InternalMapToNonNullable);
+            optional.InternalFilterNotNullOrThrow(CreateExpectedNotNullOrAbsentException);
 
         public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional, Func<Exception> exceptionFactory)
             where T : struct
         {
             _ = exceptionFactory ?? throw new ArgumentNullException(nameof(exceptionFactory));
 
-            return optional
-            .Filter(InternalCreateFilterNotNullOrThrowPredicate_Struct<T>(exceptionFactory))
-            .Map(InternalMapToNonNullable);
+            return optional.InternalFilterNotNullOrThrow(exceptionFactory);
         }
     }
 }
---
+++ src/core-taggeds-optional/Optional/Optional.Extensions.FilterNotNull/InternalFilterNotNullOrThrow.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+
+using System.Runtime.CompilerServices;
+
+namespace System
+{
+    partial class FilterNotNullOptionalExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Optional<T> InternalFilterNotNullOrThrow<T>(this Optional<T?> optional, Func<Exception> exceptionFactory)
+            =>
+            optional
+            .Filter(InternalCreateFilterNotNullOrThrowPredicate_Common<T>(exceptionFactory))
+            .Map(InternalMapToNonNullable);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Optional<T> InternalFilterNotNullOrThrow<T>(this Optional<T?> optional, Func<Exception> exceptionFactory)
+            where T : struct
+            =>
+            optional
+            .Filter(InternalCreateFilterNotNullOrThrowPredicate_Struct<T>(exceptionFactory))
+            .Map(InternalMapToNonNullable);
+    }
+}
