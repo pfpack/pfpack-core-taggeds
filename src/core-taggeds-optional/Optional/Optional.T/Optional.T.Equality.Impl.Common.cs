b767794dae86270f4bfd479db138361e6264d2bc
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-18 08:50:32 +0300

    Optional: implement the new Equality approach

--- src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.Impl.Common.cs
+++ src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.Impl.Common.cs
@@ -6,6 +6,8 @@ namespace System
 {
     partial struct Optional<T>
     {
+        private static Type EqualityContract => typeof(Optional<T>);
+
         private static IEqualityComparer<T> EqualityComparer => EqualityComparer<T>.Default;
     }
 }
---
+++ src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.Impl.GetHashCode.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+using System.Runtime.CompilerServices;
+
+namespace System
+{
+    partial struct Optional<T>
+    {
+        public override int GetHashCode()
+            =>
+            hasValue ? PresentHashCode() : AbsentHashCode();
+    
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int PresentHashCode()
+            =>
+            value is not null
+                ? HashCode.Combine(EqualityContract, true, EqualityComparer.GetHashCode(value))
+                : HashCode.Combine(EqualityContract, true);
+    
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int AbsentHashCode()
+            =>
+            HashCode.Combine(EqualityContract);
+    }
+}
