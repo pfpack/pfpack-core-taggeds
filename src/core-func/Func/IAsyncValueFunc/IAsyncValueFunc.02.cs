1d692e152f7cae41b330eea1ed8b3b269eccd1de
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-05-06 18:41:36 +0300

    Add IAsyncValueFunc; Prepare Core v1.1.1-preview.1.0.0

---
+++ src/core-func/Func/IAsyncValueFunc/IAsyncValueFunc.02.cs
@@ -0,0 +1,12 @@
+#nullable enable
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public interface IAsyncValueFunc<in T1, in T2, TResult>
+    {
+        Task<TResult> InvokeAsync(T1 arg1, T2 arg2, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/core-func/Func/IAsyncValueFunc/IAsyncValueFunc.04.cs
@@ -0,0 +1,12 @@
+#nullable enable
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public interface IAsyncValueFunc<in T1, in T2, in T3, in T4, TResult>
+    {
+        Task<TResult> InvokeAsync(T1 arg1, T2 arg2, T3 arg3, T4 arg4, CancellationToken cancellationToken = default);
+    }
+}
