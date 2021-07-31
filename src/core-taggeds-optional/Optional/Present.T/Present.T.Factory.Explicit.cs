581b5758db5e14ed455968d9925ee849b6504d9d
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-07-31 15:08:51 +0300

    Rename Present.From to Of

--- src/core-taggeds-optional/Optional/Present.T/Present.T.Factory.Explicit.cs
+++ src/core-taggeds-optional/Optional/Present.T/Present.T.Factory.Explicit.cs
@@ -4,7 +4,7 @@ namespace System
 {
     partial struct Present<T>
     {
-        public static Present<T> From(T value)
+        public static Present<T> Of(T value)
             =>
             new(value);
     }
