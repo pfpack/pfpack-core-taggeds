6216b7d8699c3f14cffe2a7cb9167db0bc884ef8
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-08-11 23:50:22 +0300

    Optional: Restore Equals

--- src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.Equals.Impl.cs
+++ src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.Equals.Impl.cs
@@ -5,23 +5,18 @@ namespace System
     partial struct Optional<T>
     {
         public bool Equals(Optional<T> other)
-            =>
-            (hasValue == other.hasValue) &&
-            (hasValue
-                ? EqualityComparer.Equals(value, other.value)
-                : true);
-        // {
-        //     if (hasValue != other.hasValue)
-        //     {
-        //         return false;
-        //     }
+        {
+            if (hasValue != other.hasValue)
+            {
+                return false;
+            }
 
-        //     if (hasValue)
-        //     {
-        //         return EqualityComparer.Equals(value, other.value);
-        //     }
+            if (hasValue)
+            {
+                return EqualityComparer.Equals(value, other.value);
+            }
 
-        //     return true;
-        // }
+            return true;
+        }
     }
 }
