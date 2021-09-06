c8f93c8155e1f678e55e1bc22a7f032082b18bc9
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-09-06 13:22:19 +0300

    Func: v1.1.2-preview.1.0.0; Func.Extensions: v1.2.0-preview.1.0.0

---
+++ src/core-func-ext/Func.Extensions/Impl.Func.Obsolete/ImplFunc.05.Obsolete.cs
@@ -0,0 +1,20 @@
+#nullable enable
+
+namespace System
+{
+    // TODO: Delete the class in v2.0
+    [Obsolete(ImplFuncObsolete.Message, error: true)]
+    public sealed class ImplFunc<T1, T2, T3, T4, T5, TResult> : IFunc<T1, T2, T3, T4, T5, TResult>
+    {
+        private readonly Func<T1, T2, T3, T4, T5, TResult> func;
+
+        public ImplFunc(
+            Func<T1, T2, T3, T4, T5, TResult> func)
+            =>
+            this.func = func;
+
+        public TResult Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+            =>
+            func.Invoke(arg1, arg2, arg3, arg4, arg5);
+    }
+}
---
+++ src/core-func-ext/Func.Extensions/Impl.Func/ImplFunc.00.cs
@@ -0,0 +1,18 @@
+#nullable enable
+
+namespace System
+{
+    internal sealed class ImplFunc<TResult> : IFunc<TResult>
+    {
+        private readonly Func<TResult> func;
+
+        internal ImplFunc(
+            Func<TResult> func)
+            =>
+            this.func = func;
+
+        public TResult Invoke()
+            =>
+            func.Invoke();
+    }
+}
