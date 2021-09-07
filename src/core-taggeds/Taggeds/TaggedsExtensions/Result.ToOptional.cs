f21c2ab764a3514d014dc439930ea186ed507e5b
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-09-07 11:07:05 +0300

    TaggedsExtensions: Fix ToOptional

--- src/core-taggeds/Taggeds/TaggedsExtensions/Result.ToOptional.cs
+++ src/core-taggeds/Taggeds/TaggedsExtensions/Result.ToOptional.cs
@@ -1,13 +1,11 @@
 ﻿#nullable enable
 
-using static System.Optional;
-
 namespace System
 {
     partial class TaggedsExtensions
     {
         public static Optional<TSuccess> ToOptional<TSuccess>(this Result<TSuccess, Unit> result)
             =>
-            result.Fold(Present<TSuccess>, Absent<TSuccess>);
+            result.Fold(Optional<TSuccess>.Present, static _ => default);
     }
 }
--- src/core-taggeds/Taggeds/TaggedsExtensions/TaggedUnion.ToOptional.cs
+++ src/core-taggeds/Taggeds/TaggedsExtensions/TaggedUnion.ToOptional.cs
@@ -1,13 +1,11 @@
 ﻿#nullable enable
 
-using static System.Optional;
-
 namespace System
 {
     partial class TaggedsExtensions
     {
         public static Optional<T> ToOptional<T>(this TaggedUnion<T, Unit> union)
             =>
-            union.Fold(Present<T>, Absent<T>);
+            union.Fold(Optional<T>.Present, static _ => default);
     }
 }
