b0f170945c6d8a930f6a8fdb5947c72deb985f39
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-10-06 23:55:41 +0300

    Failure: ToString: Use full names of the properties

--- src/core-taggeds-failure/Failure/Failure.T/Failure.T.ToString.cs
+++ src/core-taggeds-failure/Failure/Failure.T/Failure.T.ToString.cs
@@ -9,5 +9,5 @@ namespace System;
         public override string ToString()
             =>
             Invariant(
-                $"A failure of {typeof(TFailureCode)}: {{ Code: {FailureCode}, Message: \"{FailureMessage}\" }}");
+                $"A failure of {typeof(TFailureCode)}: {{ {nameof(FailureCode)}: {FailureCode}, {nameof(FailureMessage)}: \"{FailureMessage}\" }}");
     }
