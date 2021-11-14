f1f838f0addb69de9c54d940020f82a33a2ead7d
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-11-14 16:37:53 +0300

    AsyncValueFunc: Add tests for default cancellation

---
+++ src/core-func-ext-asyncvalue/Func.Extensions.AsyncValue.Tests.DefaultCancellation/AsyncValueFuncTests.2/Tests.00.cs
@@ -0,0 +1,30 @@
+using PrimeFuncPack.UnitTest;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class AsyncValueFuncTests2
+{
+    [Fact]
+    public void From_00_SourceFuncIsNull_ExpectArgumentNullException()
+    {
+        var sourceFunc = (Func<ValueTask<StructType>>)null!;
+        var ex = Assert.Throws<ArgumentNullException>(() => _ = AsyncValueFunc.From(sourceFunc));
+        Assert.Equal("funcAsync", ex.ParamName);
+    }
+
+    [Theory]
+    [MemberData(nameof(AsyncTestCaseSources.String), MemberType = typeof(AsyncTestCaseSources))]
+    public async Task From_00_ThenInvokeAsync_ExpectResultOfSourceFunc(
+        string? sourceFuncResult)
+    {
+        var actual = AsyncValueFunc.From(() => ValueTask.FromResult(sourceFuncResult));
+
+        var actualResult = await actual.InvokeAsync();
+
+        Assert.Equal(sourceFuncResult, actualResult);
+    }
+}
