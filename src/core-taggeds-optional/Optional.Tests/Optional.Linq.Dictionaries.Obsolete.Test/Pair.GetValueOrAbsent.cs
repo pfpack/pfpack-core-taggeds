426e7a5f5f1d14e7679b18c34b304c47ecfd1282
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-05 21:30:28 +0300

    Add tests for obsoletes

---
+++ src/core-taggeds-optional/Optional.Tests/Optional.Linq.Dictionaries.Obsolete.Test/Pair.GetValueOrAbsent.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using PrimeFuncPack.UnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class OptionalLinqDictionariesExtensionsObsoleteTest
+{
+    [Test]
+    public void GetValueOrAbsent_PairsAreNull_ExpectArgumentNullException()
+    {
+        IEnumerable<KeyValuePair<int, StructType>> sourcePairs = null!;
+
+        var ex = Assert.Throws<ArgumentNullException>(() => _ = OptionalLinqDictionariesExtensions.GetValueOrAbsent(sourcePairs, PlusFifteen));
+        Assert.AreEqual("pairs", ex!.ParamName);
+    }
+
+    [Test]
+    public void GetValueOrAbsent_PairsDoNotContainKey_ExpectAbsent()
+    {
+        var sourcePairs = CreatePairsCollection(
+            new KeyValuePair<int, RefType?>(MinusFifteen, ZeroIdRefType),
+            new KeyValuePair<int, RefType?>(Zero, null),
+            new KeyValuePair<int, RefType?>(int.MaxValue, PlusFifteenIdRefType));
+
+        var actual = OptionalLinqDictionariesExtensions.GetValueOrAbsent(sourcePairs, PlusFifteen);
+        var expected = Optional<RefType?>.Absent;
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCaseSource(typeof(TestDataSource), nameof(TestDataSource.ObjectNullableTestSource))]
+    public void GetValueOrAbsent_PairsContainOnlyOneKey_ExpectPresentKeyValue(
+        object? expectedValue)
+    {
+        var sourceKey = "Some Key";
+
+        var sourcePairs = CreatePairsCollection(
+            new KeyValuePair<string?, object?>(null, ZeroIdRefType),
+            new KeyValuePair<string?, object?>(SomeString, PlusFifteenIdRefType),
+            new KeyValuePair<string?, object?>(TabString, null),
+            new KeyValuePair<string?, object?>(sourceKey, expectedValue),
+            new KeyValuePair<string?, object?>(SomeString, MinusFifteenIdRefType));
+
+        var actual = OptionalLinqDictionariesExtensions.GetValueOrAbsent(sourcePairs, sourceKey);
+        var expected = Optional<object?>.Present(expectedValue);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase]
+    public void GetValueOrAbsent_PairsContainOnlyOneKeyAndKeyIsNull_ExpectPresentKeyValue()
+    {
+        var expectedValue = SomeTextStructType;
+
+        var sourcePairs = CreatePairsCollection(
+            new KeyValuePair<RefType?, StructType>(PlusFifteenIdRefType, NullTextStructType),
+            new KeyValuePair<RefType?, StructType>(null, expectedValue));
+
+        var actual = OptionalLinqDictionariesExtensions.GetValueOrAbsent(sourcePairs, null);
+        var expected = Optional<StructType>.Present(expectedValue);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void GetValueOrAbsent_PairsContainMoreThanOneKey_ExpectFirst()
+    {
+        var expectedValue1 = NullTextStructType;
+        var expectedValue2 = SomeTextStructType;
+
+        var sourcePairs = CreatePairsCollection(
+            new KeyValuePair<RefType, StructType>(PlusFifteenIdRefType, NullTextStructType),
+            new KeyValuePair<RefType, StructType>(MinusFifteenIdRefType, SomeTextStructType),
+            new KeyValuePair<RefType, StructType>(PlusFifteenIdRefType, SomeTextStructType));
+
+        var actual = OptionalLinqDictionariesExtensions.GetValueOrAbsent(sourcePairs, PlusFifteenIdRefType);
+
+        var expected1 = Optional<StructType>.Present(expectedValue1);
+        var expected2 = Optional<StructType>.Present(expectedValue2);
+
+        bool equalToAnExpected = actual.Equals(expected1) || actual.Equals(expected2);
+        Assert.True(equalToAnExpected);
+    }
+}
---
+++ src/core-taggeds-optional/Optional.Tests/Optional.Linq.Dictionaries.Obsolete.Test/Test.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace PrimeFuncPack.Core.Tests;
+
+[Obsolete]
+public sealed partial class OptionalLinqDictionariesExtensionsObsoleteTest
+{
+    private static IEnumerable<KeyValuePair<TKey, TValue>> CreatePairsCollection<TKey, TValue>(
+        params KeyValuePair<TKey, TValue>[] pairs)
+    {
+        foreach (var pair in pairs)
+        {
+            yield return pair;
+        }
+    }
+
+    private static Mock<IReadOnlyDictionary<TKey, TValue>> CreateMockReadOnlyDictionary<TKey, TValue>(
+        bool tryGetValueResult,
+        TValue returnedValue)
+    {
+        var mock = new Mock<IReadOnlyDictionary<TKey, TValue>>();
+
+        _ = mock
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            .Setup(d => d.TryGetValue(It.IsAny<TKey>(), out returnedValue))
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+            .Returns(tryGetValueResult);
+
+        return mock;
+    }
+
+    private static Mock<IDictionary<TKey, TValue>> CreateMockDictionary<TKey, TValue>(
+        bool tryGetValueResult,
+        TValue returnedValue)
+    {
+        var mock = new Mock<IDictionary<TKey, TValue>>();
+
+        _ = mock
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            .Setup(d => d.TryGetValue(It.IsAny<TKey>(), out returnedValue))
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+            .Returns(tryGetValueResult);
+
+        return mock;
+    }
+}
