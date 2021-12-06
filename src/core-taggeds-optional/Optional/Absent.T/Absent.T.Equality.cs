06746ac979c2ed659a3df189e1b949eba8c76ef8
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-06 22:27:02 +0300

    Absent Equality: Remove redundant switch

--- src/core-taggeds-optional/Optional/Absent.T/Absent.T.Equality.cs
+++ src/core-taggeds-optional/Optional/Absent.T/Absent.T.Equality.cs
@@ -2,21 +2,6 @@
 
 partial struct Absent<T>
 {
-    public static bool Equals(Absent<T> left, Absent<T> right) => (left, right) switch
-    {
-        _ => true
-    };
-
-    public static bool operator ==(Absent<T> left, Absent<T> right) => (left, right) switch
-    {
-        _ => true
-    };
-
-    public static bool operator !=(Absent<T> left, Absent<T> right) => (left, right) switch
-    {
-        _ => false
-    };
-
     public bool Equals(Absent<T> other)
         =>
         true;
