5eea94e8f3ee1befd610d3342cda1c729bb7e5b1
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-06 22:06:47 +0300

    Refactor: Add Inner prefix

--- src/core-taggeds-optional/Optional/Optional/Optional.Factory.ExceptionFactories.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System;
-
-partial class Optional
-{
-    private static ArgumentException CreateExpectedSpecifiedException(string paramName)
-        =>
-        new(message: "The value is expected to be specified.", paramName: paramName);
-}
