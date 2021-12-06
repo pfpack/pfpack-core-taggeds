5c9a8dd97d1e0973df2b1c2be7df8e712849b131
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-06 23:06:29 +0300

    Remove redundant switch

--- src/core-taggeds-optional/Optional/Absent.T/Absent.T.ToOptional.Implicit.cs
+++ src/core-taggeds-optional/Optional/Absent.T/Absent.T.ToOptional.Implicit.cs
@@ -1,9 +1,10 @@
+#pragma warning disable IDE0060 // Remove unused parameter
+
 namespace System;
 
 partial struct Absent<T>
 {
-    public static implicit operator Optional<T>(Absent<T> absent) => absent switch
-    {
-        _ => default
-    };
+    public static implicit operator Optional<T>(Absent<T> absent)
+        =>
+        default;
 }
