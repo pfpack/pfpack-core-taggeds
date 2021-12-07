1c7b8b784ee311975d6c526a5cb7cb56de407c8b
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-07 20:22:17 +0300

    Optional: Refactor Fold / FlatMap

--- src/core-taggeds-optional/Optional/Optional.T/Inner/Optional.T.InnerBindOrFlatMap.cs
+++ src/core-taggeds-optional/Optional/Optional.T/Inner/Optional.T.InnerBindOrFlatMap.cs
@@ -6,17 +6,20 @@ namespace System;
 partial struct Optional<T>
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private Optional<TResult> InnerBindOrFlatMap<TResult>(Func<T, Optional<TResult>> map)
+    private Optional<TResult> InnerBindOrFlatMap<TResult>(
+        Func<T, Optional<TResult>> map)
         =>
         InnerFold(map, static () => default);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private Task<Optional<TResult>> InnerBindOrFlatMapAsync<TResult>(Func<T, Task<Optional<TResult>>> mapAsync)
+    private Task<Optional<TResult>> InnerBindOrFlatMapAsync<TResult>(
+        Func<T, Task<Optional<TResult>>> mapAsync)
         =>
         InnerFold(mapAsync, static () => Task.FromResult<Optional<TResult>>(default));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private ValueTask<Optional<TResult>> InnerBindOrFlatMapValueAsync<TResult>(Func<T, ValueTask<Optional<TResult>>> mapAsync)
+    private ValueTask<Optional<TResult>> InnerBindOrFlatMapValueAsync<TResult>(
+        Func<T, ValueTask<Optional<TResult>>> mapAsync)
         =>
         InnerFold(mapAsync, static () => default);
 }
