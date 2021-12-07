8dff0b2fff0eb69ac7b29fbc11eb05f589d96729
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-07 21:20:24 +0300

    Refactor InnerBindOrFlatMap

--- src/core-taggeds-optional/Optional/Optional.T/Inner/Optional.T.InnerBindOrFlatMap.cs
+++ src/core-taggeds-optional/Optional/Optional.T/Inner/Optional.T.InnerBindOrFlatMap.cs
@@ -9,17 +9,17 @@ partial struct Optional<T>
     private Optional<TResult> InnerBindOrFlatMap<TResult>(
         Func<T, Optional<TResult>> map)
         =>
-        InnerFold(map, static () => default);
+        hasValue ? map.Invoke(value) : default;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private Task<Optional<TResult>> InnerBindOrFlatMapAsync<TResult>(
         Func<T, Task<Optional<TResult>>> mapAsync)
         =>
-        InnerFold(mapAsync, static () => Task.FromResult<Optional<TResult>>(default));
+        hasValue ? mapAsync.Invoke(value) : Task.FromResult<Optional<TResult>>(default);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private ValueTask<Optional<TResult>> InnerBindOrFlatMapValueAsync<TResult>(
         Func<T, ValueTask<Optional<TResult>>> mapAsync)
         =>
-        InnerFold(mapAsync, static () => default);
+        hasValue ? mapAsync.Invoke(value) : default;
 }
