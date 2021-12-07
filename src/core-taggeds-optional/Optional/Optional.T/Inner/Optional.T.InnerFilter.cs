6ecd3e7d6f45d459cb73833e8bdb6e54418e65b5
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-07 19:36:41 +0300

    Refactor Filter

---
+++ src/core-taggeds-optional/Optional/Optional.T/Inner/Optional.T.InnerFilter.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+
+namespace System;
+
+partial struct Optional<T>
+{
+    private Optional<T> InnerFilter(Func<T, bool> predicate)
+    {
+        if (hasValue is false)
+        {
+            return this;
+        }
+
+        if (predicate.Invoke(value))
+        {
+            return this;
+        }
+
+        return default;
+    }
+
+    private async Task<Optional<T>> InnerFilterAsync(Func<T, Task<bool>> predicateAsync)
+    {
+        if (hasValue is false)
+        {
+            return this;
+        }
+
+        if (await predicateAsync.Invoke(value).ConfigureAwait(false))
+        {
+            return this;
+        }
+
+        return default;
+    }
+
+    private async ValueTask<Optional<T>> InnerFilterValueAsync(Func<T, ValueTask<bool>> predicateAsync)
+    {
+        if (hasValue is false)
+        {
+            return this;
+        }
+
+        if (await predicateAsync.Invoke(value).ConfigureAwait(false))
+        {
+            return this;
+        }
+
+        return default;
+    }
+}
