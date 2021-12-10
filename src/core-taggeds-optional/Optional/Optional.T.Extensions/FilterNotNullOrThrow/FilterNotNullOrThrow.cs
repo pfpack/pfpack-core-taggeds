08bef1a5c27a7c31e50260355ae65979070d36e9
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-10 19:36:52 +0300

    Arrange OptionalExtensions

---
+++ src/core-taggeds-optional/Optional/Optional.T.Extensions/FilterNotNullOrThrow/FilterNotNullOrThrow.cs
@@ -0,0 +1,32 @@
+﻿namespace System;
+
+partial class OptionalExtensions
+{
+    public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional)
+        =>
+        optional.FlatMap(
+            value => InnerFilterNotNullOrThrowMap(value, InnerCreateExpectedNotNullException));
+
+    public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional)
+        where T : struct
+        =>
+        optional.FlatMap(
+            value => InnerFilterNotNullOrThrowMap(value, InnerCreateExpectedNotNullException));
+
+    public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional, Func<Exception> exceptionFactory)
+    {
+        _ = exceptionFactory ?? throw new ArgumentNullException(nameof(exceptionFactory));
+
+        return optional.FlatMap(
+            value => InnerFilterNotNullOrThrowMap(value, exceptionFactory));
+    }
+
+    public static Optional<T> FilterNotNullOrThrow<T>(this Optional<T?> optional, Func<Exception> exceptionFactory)
+        where T : struct
+    {
+        _ = exceptionFactory ?? throw new ArgumentNullException(nameof(exceptionFactory));
+
+        return optional.FlatMap(
+            value => InnerFilterNotNullOrThrowMap(value, exceptionFactory));
+    }
+}
