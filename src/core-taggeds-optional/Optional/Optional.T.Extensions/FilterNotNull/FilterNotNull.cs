118e711dae07cc401c904df1aa33ab8dbca21f74
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-11 11:15:56 +0300

    Refactor OptionalExtensions

--- src/core-taggeds-optional/Optional/Optional.T.Extensions/FilterNotNull/FilterNotNull.cs
+++ src/core-taggeds-optional/Optional/Optional.T.Extensions/FilterNotNull/FilterNotNull.cs
@@ -4,16 +4,10 @@ partial class OptionalExtensions
 {
     public static Optional<T> FilterNotNull<T>(this Optional<T?> optional)
         =>
-        optional.FlatMap<T>(
-            value => value is not null
-                ? new(value)
-                : default);
+        optional.FlatMap(InnerFilterNotNullMap);
 
     public static Optional<T> FilterNotNull<T>(this Optional<T?> optional)
         where T : struct
         =>
-        optional.FlatMap<T>(
-            value => value is not null
-                ? new(value.GetValueOrDefault())
-                : default);
+        optional.FlatMap(InnerFilterNotNullMap);
 }
