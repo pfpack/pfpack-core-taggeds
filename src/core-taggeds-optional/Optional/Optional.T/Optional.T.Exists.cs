f453b3161a6c7dc14334a9601b96e5862ff1ca47
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-12 13:52:40 +0300

    Implement Optional Exists

---
+++ src/core-taggeds-optional/Optional/Optional.T/Optional.T.Exists.cs
@@ -0,0 +1,9 @@
+﻿namespace System;
+
+partial struct Optional<T>
+{
+    // TODO: Add the tests and open the method
+    internal bool Exists(Func<T, bool> predicate)
+        =>
+        InnerExists(predicate);
+}
