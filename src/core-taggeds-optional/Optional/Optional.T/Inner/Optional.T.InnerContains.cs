6104cdcde3ca0c94d3b99e0d75b055cc07aaec2f
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-12 10:17:23 +0300

    Optional: Add Contains

---
+++ src/core-taggeds-optional/Optional/Optional.T/Inner/Optional.T.InnerContains.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System;
+
+partial struct Optional<T>
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool InnerContains(T value)
+        =>
+        hasValue && EqualityComparer.Equals(this.value, value);
+}
