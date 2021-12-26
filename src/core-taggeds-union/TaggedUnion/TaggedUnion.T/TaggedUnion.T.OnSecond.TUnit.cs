32abe8c18308c3f02cfa038a69318f8784e2a815
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-27 00:38:36 +0300

    TaggedUnion: Implement/arrange OnSecond methods

--- src/core-taggeds-union/TaggedUnion/TaggedUnion.T/TaggedUnion.T.OnSecond.TUnit.cs
+++ src/core-taggeds-union/TaggedUnion/TaggedUnion.T/TaggedUnion.T.OnSecond.TUnit.cs
@@ -5,20 +5,20 @@ namespace System;
 partial struct TaggedUnion<TFirst, TSecond>
 {
     // TODO: Add the tests and open the methods
-    internal TaggedUnion<TFirst, TSecond> OnSecond(
-        Action<TSecond> handler)
+    internal TaggedUnion<TFirst, TSecond> OnSecond<TUnit>(
+        Func<TSecond, TUnit> handler)
         =>
         InnerOnSecond(
             handler ?? throw new ArgumentNullException(nameof(handler)));
 
-    internal Task<TaggedUnion<TFirst, TSecond>> OnSecondAsync(
-        Func<TSecond, Task> handlerAsync)
+    internal Task<TaggedUnion<TFirst, TSecond>> OnSecondAsync<TUnit>(
+        Func<TSecond, Task<TUnit>> handlerAsync)
         =>
         InnerOnSecondAsync(
             handlerAsync ?? throw new ArgumentNullException(nameof(handlerAsync)));
 
-    internal ValueTask<TaggedUnion<TFirst, TSecond>> OnSecondValueAsync(
-        Func<TSecond, ValueTask> handlerAsync)
+    internal ValueTask<TaggedUnion<TFirst, TSecond>> OnSecondValueAsync<TUnit>(
+        Func<TSecond, ValueTask<TUnit>> handlerAsync)
         =>
         InnerOnSecondValueAsync(
             handlerAsync ?? throw new ArgumentNullException(nameof(handlerAsync)));
--- src/core-taggeds-union/TaggedUnion/TaggedUnion.T/TaggedUnion.T.OnSecond.Unit.cs
+++ src/core-taggeds-union/TaggedUnion/TaggedUnion.T/TaggedUnion.T.OnSecond.Unit.cs
@@ -6,19 +6,19 @@ partial struct TaggedUnion<TFirst, TSecond>
 {
     // TODO: Add the tests and open the methods
     internal TaggedUnion<TFirst, TSecond> OnSecond(
-        Action<TSecond> handler)
+        Func<TSecond, Unit> handler)
         =>
         InnerOnSecond(
             handler ?? throw new ArgumentNullException(nameof(handler)));
 
     internal Task<TaggedUnion<TFirst, TSecond>> OnSecondAsync(
-        Func<TSecond, Task> handlerAsync)
+        Func<TSecond, Task<Unit>> handlerAsync)
         =>
         InnerOnSecondAsync(
             handlerAsync ?? throw new ArgumentNullException(nameof(handlerAsync)));
 
     internal ValueTask<TaggedUnion<TFirst, TSecond>> OnSecondValueAsync(
-        Func<TSecond, ValueTask> handlerAsync)
+        Func<TSecond, ValueTask<Unit>> handlerAsync)
         =>
         InnerOnSecondValueAsync(
             handlerAsync ?? throw new ArgumentNullException(nameof(handlerAsync)));
