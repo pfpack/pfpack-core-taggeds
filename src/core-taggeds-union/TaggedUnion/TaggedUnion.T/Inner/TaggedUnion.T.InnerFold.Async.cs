1cbe4d720af01d863fa81d4751c2f2f891b00ef7
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-27 19:32:40 +0300

    TaggedUnion: Rework InnerFold methods

---
+++ src/core-taggeds-union/TaggedUnion/TaggedUnion.T/Inner/TaggedUnion.T.InnerFold.Async.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace System;
+
+partial struct TaggedUnion<TFirst, TSecond>
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private async Task<TResult?> InnerFoldAsync<TResult>(
+        Func<TFirst, Task<TResult>> mapFirstAsync,
+        Func<TSecond, Task<TResult>> mapSecondAsync)
+    {
+        if (tag is InternalTag.First)
+        {
+            return await mapFirstAsync.Invoke(first).ConfigureAwait(false);
+        }
+
+        if (tag is InternalTag.Second)
+        {
+            return await mapSecondAsync.Invoke(second).ConfigureAwait(false);
+        }
+
+        return default;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private Task<TResult> InnerFoldAsync<TResult>(
+        Func<TFirst, Task<TResult>> mapFirstAsync,
+        Func<TSecond, Task<TResult>> mapSecondAsync,
+        TResult other)
+    {
+        if (tag is InternalTag.First)
+        {
+            return mapFirstAsync.Invoke(first);
+        }
+
+        if (tag is InternalTag.Second)
+        {
+            return mapSecondAsync.Invoke(second);
+        }
+
+        return Task.FromResult(other);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private Task<TResult> InnerFoldAsync<TResult>(
+        Func<TFirst, Task<TResult>> mapFirstAsync,
+        Func<TSecond, Task<TResult>> mapSecondAsync,
+        Func<TResult> otherFactory)
+    {
+        if (tag is InternalTag.First)
+        {
+            return mapFirstAsync.Invoke(first);
+        }
+
+        if (tag is InternalTag.Second)
+        {
+            return mapSecondAsync.Invoke(second);
+        }
+
+        return Task.FromResult(otherFactory.Invoke());
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private Task<TResult> InnerFoldAsync<TResult>(
+        Func<TFirst, Task<TResult>> mapFirstAsync,
+        Func<TSecond, Task<TResult>> mapSecondAsync,
+        Func<Task<TResult>> otherFactoryAsync)
+    {
+        if (tag is InternalTag.First)
+        {
+            return mapFirstAsync.Invoke(first);
+        }
+
+        if (tag is InternalTag.Second)
+        {
+            return mapSecondAsync.Invoke(second);
+        }
+
+        return otherFactoryAsync.Invoke();
+    }
+}
---
+++ src/core-taggeds-union/TaggedUnion/TaggedUnion.T/TaggedUnion.T.Fold.Async.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+namespace System;
+
+partial struct TaggedUnion<TFirst, TSecond>
+{
+    public Task<TResult> FoldAsync<TResult>(
+        Func<TFirst, Task<TResult>> mapFirstAsync,
+        Func<TSecond, Task<TResult>> mapSecondAsync,
+        TResult other)
+        =>
+        InnerFoldAsync(
+            mapFirstAsync ?? throw new ArgumentNullException(nameof(mapFirstAsync)),
+            mapSecondAsync ?? throw new ArgumentNullException(nameof(mapSecondAsync)),
+            other);
+
+    public Task<TResult> FoldAsync<TResult>(
+        Func<TFirst, Task<TResult>> mapFirstAsync,
+        Func<TSecond, Task<TResult>> mapSecondAsync,
+        Func<TResult> otherFactory)
+        =>
+        InnerFoldAsync(
+            mapFirstAsync ?? throw new ArgumentNullException(nameof(mapFirstAsync)),
+            mapSecondAsync ?? throw new ArgumentNullException(nameof(mapSecondAsync)),
+            otherFactory ?? throw new ArgumentNullException(nameof(otherFactory)));
+
+    public Task<TResult> FoldAsync<TResult>(
+        Func<TFirst, Task<TResult>> mapFirstAsync,
+        Func<TSecond, Task<TResult>> mapSecondAsync,
+        Func<Task<TResult>> otherFactoryAsync)
+        =>
+        InnerFoldAsync(
+            mapFirstAsync ?? throw new ArgumentNullException(nameof(mapFirstAsync)),
+            mapSecondAsync ?? throw new ArgumentNullException(nameof(mapSecondAsync)),
+            otherFactoryAsync ?? throw new ArgumentNullException(nameof(otherFactoryAsync)));
+}
