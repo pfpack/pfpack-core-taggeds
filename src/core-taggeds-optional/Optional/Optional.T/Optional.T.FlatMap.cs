b9db0334dec7d9ad304f64e144bc2c439842b5a2
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-28 15:16:02 +0300

    Format code

--- src/core-taggeds-optional/Optional/Optional.T/Optional.T.FlatMap.cs
+++ src/core-taggeds-optional/Optional/Optional.T/Optional.T.FlatMap.cs
@@ -7,15 +7,18 @@ partial struct Optional<T>
     public Optional<TResult> FlatMap<TResult>(
         Func<T, Optional<TResult>> map)
         =>
-        InnerBindOrFlatMap(map ?? throw new ArgumentNullException(nameof(map)));
+        InnerBindOrFlatMap(
+            map ?? throw new ArgumentNullException(nameof(map)));
 
     public Task<Optional<TResult>> FlatMapAsync<TResult>(
         Func<T, Task<Optional<TResult>>> mapAsync)
         =>
-        InnerBindOrFlatMapAsync(mapAsync ?? throw new ArgumentNullException(nameof(mapAsync)));
+        InnerBindOrFlatMapAsync(
+            mapAsync ?? throw new ArgumentNullException(nameof(mapAsync)));
 
     public ValueTask<Optional<TResult>> FlatMapValueAsync<TResult>(
         Func<T, ValueTask<Optional<TResult>>> mapAsync)
         =>
-        InnerBindOrFlatMapValueAsync(mapAsync ?? throw new ArgumentNullException(nameof(mapAsync)));
+        InnerBindOrFlatMapValueAsync(
+            mapAsync ?? throw new ArgumentNullException(nameof(mapAsync)));
 }
