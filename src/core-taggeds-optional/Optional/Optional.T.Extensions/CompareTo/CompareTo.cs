1500b59de534a5c6f09ada19783edb92dbe51c1c
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-01-01 18:05:55 +0300

    Optional: Add Comparison

---
+++ src/core-taggeds-optional/Optional/Optional.T.Extensions/CompareTo/CompareTo.cs
@@ -0,0 +1,10 @@
+﻿namespace System;
+
+partial class OptionalExtensions
+{
+    // TODO: Add the tests and open the method
+    internal static int CompareTo<T>(this Optional<T> optional, Optional<T> other)
+        where T : IComparable<T>
+        =>
+        optional.InternalCompareTo(other);
+}
