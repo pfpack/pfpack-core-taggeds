3fd7998e7821ffd0479385332ccda519fa6a82f8
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-01-01 22:17:30 +0300

    Simplify Inner Flatten

--- src/core-taggeds-optional/Optional/Optional.T.Extensions/Flatten.Inner/InnerFlattenPipe.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace System;
-
-partial class OptionalExtensions
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static Optional<T> InnerFlattenPipe<T>(Optional<T> optional) => optional;
-}
