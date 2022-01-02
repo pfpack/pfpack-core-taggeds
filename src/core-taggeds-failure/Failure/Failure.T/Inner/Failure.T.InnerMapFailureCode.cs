adf6a8bdf448892e0fb10d60054c3f2cc398a8c0
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-01-02 11:03:43 +0300

    Failure: Add AggressiveInlining to an Inner

--- src/core-taggeds-failure/Failure/Failure.T/Inner/Failure.T.InnerMapFailureCode.cs
+++ src/core-taggeds-failure/Failure/Failure.T/Inner/Failure.T.InnerMapFailureCode.cs
@@ -1,7 +1,10 @@
+using System.Runtime.CompilerServices;
+
 namespace System;
 
 partial struct Failure<TFailureCode>
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private Failure<TResultFailureCode> InnerMapFailureCode<TResultFailureCode>(
         Func<TFailureCode, TResultFailureCode> mapFailureCode)
         where TResultFailureCode : struct
