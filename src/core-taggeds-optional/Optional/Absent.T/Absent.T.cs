a9ad0389c6259d0d402cf72ec2406a673dee2eb9
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-01-02 14:37:09 +0300

    Absent: Invert inherited interfaces declaration order (for performance reasons)

--- src/core-taggeds-optional/Optional/Absent.T/Absent.T.cs
+++ src/core-taggeds-optional/Optional/Absent.T/Absent.T.cs
@@ -1,6 +1,9 @@
 ﻿namespace System;
 
 // TODO: Add the tests and open the type
-internal readonly partial struct Absent<T> : IEquatable<Absent<T>>, IComparable<Absent<T>>, IComparable
+internal readonly partial struct Absent<T> :
+    IComparable,
+    IComparable<Absent<T>>,
+    IEquatable<Absent<T>>
 {
 }
