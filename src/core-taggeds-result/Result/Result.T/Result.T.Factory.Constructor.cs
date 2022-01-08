7acb30f997c9f810e4559b04303b17f59e9cee87
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-01-08 12:07:46 +0300

    Result: Minor constructor refactor

--- src/core-taggeds-result/Result/Result.T/Result.T.Factory.Constructor.cs
+++ src/core-taggeds-result/Result/Result.T/Result.T.Factory.Constructor.cs
@@ -4,9 +4,9 @@ partial struct Result<TSuccess, TFailure>
 {
     public Result(TSuccess success)
         =>
-        (this.success, failure, isSuccess) = (success, default, true);
+        (this.success, isSuccess, failure) = (success, true, default);
 
     public Result(TFailure failure)
         =>
-        (this.failure, success, isSuccess) = (failure, default!, false);
+        (this.failure, isSuccess, success) = (failure, false, default!);
 }
