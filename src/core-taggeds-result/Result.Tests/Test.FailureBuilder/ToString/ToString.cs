091371bf1fe5b2e32edac3698a6c124ed929f312
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-01-14 10:52:23 +0300

    Result Builders: Update/add ToString tests

--- src/core-taggeds-result/Result.Tests/Test.FailureBuilder/ToString/ToString.cs
+++ src/core-taggeds-result/Result.Tests/Test.FailureBuilder/ToString/ToString.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using static PrimeFuncPack.UnitTest.TestData;
 
 namespace PrimeFuncPack.Core.Tests;
@@ -7,27 +9,88 @@ namespace PrimeFuncPack.Core.Tests;
 partial class FailureBuilderTest
 {
     [Test]
-    public void ToString_SourceFailureToStringReturnsNull_ExpectEmpty()
+    public void ToString_SourceFailureToStringReturnsNull()
     {
         var sourceFailure = new StubToStringStructType(null);
         var builder = Result.Failure(sourceFailure);
-
         var actual = builder.ToString();
-        Assert.IsEmpty(actual);
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "FailureBuilder[{0}]:{1}",
+            typeof(StubToStringStructType),
+            string.Empty);
+
+        Assert.AreEqual(expected, actual);
     }
 
     [Test]
+    [TestCase(null)]
     [TestCase(EmptyString)]
-    [TestCase(WhiteSpaceString)]
     [TestCase(TabString)]
+    [TestCase(TwoTabsString)]
+    [TestCase(WhiteSpaceString)]
+    [TestCase(TwoWhiteSpacesString)]
+    [TestCase(ThreeWhiteSpacesString)]
+    [TestCase(MixedWhiteSpacesString)]
     [TestCase(SomeString)]
-    public void ToString_SourceFailureToStringReturnsNotNull_ExpectResultOfSourceFailureToString(
-        string resultOfFailureToString)
+    public void ToString_ValueToString_Common(
+        string? resultOfFailureToString)
     {
         var sourceFailure = new StubToStringStructType(resultOfFailureToString);
         var builder = Result.Failure(sourceFailure);
-
         var actual = builder.ToString();
-        Assert.AreEqual(resultOfFailureToString, actual);
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "FailureBuilder[{0}]:{1}",
+            typeof(StubToStringStructType),
+            resultOfFailureToString);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCase(null)]
+    [TestCase(MinusOne)]
+    [TestCase(Zero)]
+    [TestCase(One)]
+    public void ToString_Common(
+        int sourceFailure)
+    {
+        var source = Result.Failure(sourceFailure);
+        var actual = source.ToString();
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "FailureBuilder[{0}]:{1}",
+            typeof(int),
+            sourceFailure);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(ToString_DecimalPoint_TestCaseSource))]
+    public void ToString_DecimalPoint(
+        decimal sourceFailure, string expectedDecimalSubstr)
+    {
+        var source = Result.Failure(sourceFailure);
+        var actual = source.ToString();
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "FailureBuilder[{0}]:{1}",
+            typeof(decimal),
+            expectedDecimalSubstr);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    private static IEnumerable<object[]> ToString_DecimalPoint_TestCaseSource()
+    {
+        yield return new object[] { -1.1m, "-1.1" };
+        yield return new object[] { 0.0m, "0.0" };
+        yield return new object[] { 1.1m, "1.1" };
     }
 }
--- src/core-taggeds-result/Result.Tests/Test.SuccessBuilder/ToString/ToString.cs
+++ src/core-taggeds-result/Result.Tests/Test.SuccessBuilder/ToString/ToString.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using PrimeFuncPack.UnitTest;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using static PrimeFuncPack.UnitTest.TestData;
 
 namespace PrimeFuncPack.Core.Tests;
@@ -8,36 +10,111 @@ namespace PrimeFuncPack.Core.Tests;
 partial class SuccessBuilderTest
 {
     [Test]
-    public void ToString_SourceSuccessIsNull_ExpectEmpty()
+    public void ToString_SourceSuccessIsNull()
     {
         var builder = Result.Success<RefType?>(null);
-
         var actual = builder.ToString();
-        Assert.IsEmpty(actual);
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "SuccessBuilder[{0}]:{1}",
+            typeof(RefType),
+            string.Empty);
+
+        Assert.AreEqual(expected, actual);
     }
 
     [Test]
-    public void ToString_SourceSuccessToStringReturnsNull_ExpectEmpty()
+    public void ToString_SourceSuccessToStringReturnsNull()
     {
         var sourceSuccess = new StubToStringRefType(null);
         var builder = Result.Success(sourceSuccess);
-
         var actual = builder.ToString();
-        Assert.IsEmpty(actual);
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "SuccessBuilder[{0}]:{1}",
+            typeof(StubToStringRefType),
+            string.Empty);
+
+        Assert.AreEqual(expected, actual);
     }
 
     [Test]
+    [TestCase(null)]
     [TestCase(EmptyString)]
-    [TestCase(WhiteSpaceString)]
     [TestCase(TabString)]
+    [TestCase(TwoTabsString)]
+    [TestCase(WhiteSpaceString)]
+    [TestCase(TwoWhiteSpacesString)]
+    [TestCase(ThreeWhiteSpacesString)]
+    [TestCase(MixedWhiteSpacesString)]
     [TestCase(SomeString)]
-    public void ToString_SourceSuccessToStringReturnsNotNull_ExpectResultOfSourceSuccessToString(
-        string resultOfSuccessToString)
+    public void ToString_SourceSuccess_ValueToString_Common(
+        string? resultOfSuccessToString)
     {
         var sourceSuccess = new StubToStringRefType(resultOfSuccessToString);
         var builder = Result.Success(sourceSuccess);
-
         var actual = builder.ToString();
-        Assert.AreEqual(resultOfSuccessToString, actual);
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "SuccessBuilder[{0}]:{1}",
+            typeof(StubToStringRefType),
+            resultOfSuccessToString);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCase(null)]
+    [TestCase(EmptyString)]
+    [TestCase(TabString)]
+    [TestCase(TwoTabsString)]
+    [TestCase(WhiteSpaceString)]
+    [TestCase(TwoWhiteSpacesString)]
+    [TestCase(ThreeWhiteSpacesString)]
+    [TestCase(MixedWhiteSpacesString)]
+    [TestCase(SomeString)]
+    [TestCase(MinusOne)]
+    [TestCase(Zero)]
+    [TestCase(One)]
+    public void ToString_Common(
+        object? sourceSuccess)
+    {
+        var source = Result.Success(sourceSuccess);
+        var actual = source.ToString();
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "SuccessBuilder[{0}]:{1}",
+            typeof(object),
+            sourceSuccess);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(ToString_DecimalPoint_TestCaseSource))]
+    public void ToString_DecimalPoint(
+        decimal sourceSuccess, string expectedDecimalSubstr)
+    {
+        var source = Result.Success(sourceSuccess);
+        var actual = source.ToString();
+
+        var expected = string.Format(
+            CultureInfo.InvariantCulture,
+            "SuccessBuilder[{0}]:{1}",
+            typeof(decimal),
+            expectedDecimalSubstr);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    private static IEnumerable<object[]> ToString_DecimalPoint_TestCaseSource()
+    {
+        yield return new object[] { -1.1m, "-1.1" };
+        yield return new object[] { 0.0m, "0.0" };
+        yield return new object[] { 1.1m, "1.1" };
     }
 }
