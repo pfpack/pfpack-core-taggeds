cdf267b0485a13e04c78aaed69d801c7afc32449
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-05-15 09:57:08 +0300

    Improve use of EqualityContract in GetHashCode

--- src/core-taggeds-union/TaggedUnion/TaggedUnion.T/TaggedUnion.T.Equality.Common.cs
+++ src/core-taggeds-union/TaggedUnion/TaggedUnion.T/TaggedUnion.T.Equality.Common.cs
@@ -4,9 +4,19 @@ namespace System;
 
 partial struct TaggedUnion<TFirst, TSecond>
 {
-    private static Type EqualityContract => typeof(TaggedUnion<TFirst, TSecond>);
+    private static Type EqualityContract
+        =>
+        typeof(TaggedUnion<TFirst, TSecond>);
 
-    private static EqualityComparer<TFirst> FirstComparer => EqualityComparer<TFirst>.Default;
+    private static EqualityComparer<Type> EqualityContractComparer
+        =>
+        EqualityComparer<Type>.Default;
 
-    private static EqualityComparer<TSecond> SecondComparer => EqualityComparer<TSecond>.Default;
+    private static EqualityComparer<TFirst> FirstComparer
+        =>
+        EqualityComparer<TFirst>.Default;
+
+    private static EqualityComparer<TSecond> SecondComparer
+        =>
+        EqualityComparer<TSecond>.Default;
 }
