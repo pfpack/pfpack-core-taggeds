5e5e79bece74b932c776316a636bf7914719aa2e
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-03-04 11:35:50 +0400

    Refactor: rename a param

--- src/core-taggeds-optional/Optional/OptionalExtensions/CompareTo/CompareTo.cs
+++ src/core-taggeds-optional/Optional/OptionalExtensions/CompareTo/CompareTo.cs
@@ -5,9 +5,9 @@ namespace System;
 partial class OptionalExtensions
 {
     // TODO: Add the tests and open the method
-    internal static int CompareTo<T>(this Optional<T> optional, Optional<T> other) where T : IComparable<T>
+    internal static int CompareTo<T>(this Optional<T> optional, Optional<T> otherOptional) where T : IComparable<T>
         =>
-        optional.InternalCompareTo(other, Comparer<T>.Default);
+        optional.InternalCompareTo(otherOptional, Comparer<T>.Default);
 
     // TODO: Add the tests and open the method
     internal static int CompareTo<T>(this Optional<T> optional, object? obj) where T : IComparable<T>
