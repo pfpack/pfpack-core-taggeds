c0175c2de18ebb6904d8017d54454754b8616cd4
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-03-04 14:26:42 +0400

    Minor refactor: rename

--- src/core-taggeds-optional/Optional/OptionalExtensions/CompareTo/CompareTo.cs
+++ src/core-taggeds-optional/Optional/OptionalExtensions/CompareTo/CompareTo.cs
@@ -5,9 +5,9 @@ namespace System;
 partial class OptionalExtensions
 {
     // TODO: Add the tests and open the method
-    internal static int CompareTo<T>(this Optional<T> optional, Optional<T> otherOptional) where T : IComparable<T>
+    internal static int CompareTo<T>(this Optional<T> optional, Optional<T> other) where T : IComparable<T>
         =>
-        optional.InternalCompareTo(otherOptional, Comparer<T>.Default);
+        optional.InternalCompareTo(other, Comparer<T>.Default);
 
     // TODO: Add the tests and open the method
     internal static int CompareTo<T>(this Optional<T> optional, object? obj) where T : IComparable<T>
@@ -16,7 +16,7 @@ partial class OptionalExtensions
         {
             null => ComparisonResult.GreaterThan,
 
-            Optional<T> otherOptional => optional.CompareTo(otherOptional),
+            Optional<T> other => optional.CompareTo(other),
 
             _ => throw new ArgumentException($"The object is not Optional<{typeof(T).Name}>.", nameof(obj))
         };
