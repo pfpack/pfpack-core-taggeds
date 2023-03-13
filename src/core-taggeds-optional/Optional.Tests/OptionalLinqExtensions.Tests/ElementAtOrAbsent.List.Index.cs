8b3482563bba366356ce3f34f51b2d9472957f2b
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-03-13 23:23:32 +0400

    Add ElementAtOrAbsent Index FromStart tests

---
+++ src/core-taggeds-optional/Optional.Tests/OptionalLinqExtensions.Tests/ElementAtOrAbsent.List.Index.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using PrimeFuncPack.UnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class OptionalLinqExtensionsTests
+{
+    [Test]
+    public void ElementAtOrAbsentByIndex_ListSourceIsNull_ExpectArgumentNullException()
+    {
+        IList<RefType> source = null!;
+        var index = Index.FromStart(1);
+
+        var ex = Assert.Throws<ArgumentNullException>(Test);
+        Assert.AreEqual("source", ex?.ParamName);
+
+        void Test()
+            =>
+            _ = source.ElementAtOrAbsent(index);
+    }
+
+    [Test]
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    public void ElementAtOrAbsentByIndex_ListIndexIsInRange_ExpectPresentItem(
+        int indexValue)
+    {
+        var index = Index.FromStart(indexValue);
+
+        var source = CreateList<StructType?>(SomeTextStructType, null, NullTextStructType);
+        var actual = source.ElementAtOrAbsent(index);
+
+        var expectedValue = source.ElementAt(index);
+        var expected = Optional<StructType?>.Present(expectedValue);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCase(2)]
+    [TestCase(int.MaxValue)]
+    public void ElementAtOrAbsentByIndex_ListIndexIsNotInRange_ExpectAbsent(
+        int indexValue)
+    {
+        var index = Index.FromStart(indexValue);
+
+        var source = CreateList(SomeTextStructType, default);
+
+        var actual = source.ElementAtOrAbsent(index);
+        var expected = Optional<StructType>.Absent;
+
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ src/core-taggeds-optional/Optional.Tests/OptionalLinqExtensions.Tests/ElementAtOrAbsent.ReadOnlyList.Index.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using PrimeFuncPack.UnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class OptionalLinqExtensionsTests
+{
+    [Test]
+    public void ElementAtOrAbsentByIndex_ReadOnlyListSourceIsNull_ExpectArgumentNullException()
+    {
+        IReadOnlyList<StructType> source = null!;
+        var index = Index.FromStart(1);
+
+        var ex = Assert.Throws<ArgumentNullException>(Test);
+        Assert.AreEqual("source", ex?.ParamName);
+
+        void Test()
+            =>
+            _ = source.ElementAtOrAbsent(index);
+    }
+
+    [Test]
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    public void ElementAtOrAbsentByIndex_ReadOnlyListIndexIsInRange_ExpectPresentItem(
+        int indexValue)
+    {
+        var index = Index.FromStart(indexValue);
+
+        var source = CreateReadOnlyList(null, MinusFifteenIdRefType, ZeroIdRefType, PlusFifteenIdRefType);
+        var actual = source.ElementAtOrAbsent(index);
+
+        var expectedValue = source.ElementAt(index);
+        var expected = Optional<RefType?>.Present(expectedValue);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCase(2)]
+    [TestCase(int.MaxValue)]
+    public void ElementAtOrAbsentByIndex_ReadOnlyListIndexIsNotInRange_ExpectAbsent(
+        int indexValue)
+    {
+        var index = Index.FromStart(indexValue);
+
+        var source = CreateReadOnlyList(SomeTextStructType, default);
+
+        var actual = source.ElementAtOrAbsent(index);
+        var expected = Optional<StructType>.Absent;
+
+        Assert.AreEqual(expected, actual);
+    }
+}
