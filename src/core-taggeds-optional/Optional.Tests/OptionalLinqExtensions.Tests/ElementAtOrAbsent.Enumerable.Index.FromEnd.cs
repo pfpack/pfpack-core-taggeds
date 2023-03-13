91b45058e0b02ddf3666f0373c954e87c670fb12
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-03-13 23:30:02 +0400

    Add ElementAtOrAbsent Index FromEnd tests

---
+++ src/core-taggeds-optional/Optional.Tests/OptionalLinqExtensions.Tests/ElementAtOrAbsent.Enumerable.Index.FromEnd.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using PrimeFuncPack.UnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class OptionalLinqExtensionsTests
+{
+    [Test]
+    public void ElementAtOrAbsentByIndexFromEnd_CollectionSourceIsNull_ExpectArgumentNullException()
+    {
+        IEnumerable<StructType> source = null!;
+        var index = Index.FromEnd(1);
+
+        var ex = Assert.Throws<ArgumentNullException>(() => _ = source.ElementAtOrAbsent(index));
+        Assert.AreEqual("source", ex?.ParamName);
+    }
+
+    [Test]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    public void ElementAtOrAbsentByIndexFromEnd_CollectionIndexIsInRange_ExpectPresentItem(
+        int indexValue)
+    {
+        var index = Index.FromEnd(indexValue);
+
+        var source = CreateCollection(PlusFifteenIdRefType, null, ZeroIdRefType);
+        var actual = source.ElementAtOrAbsent(index);
+
+        var expectedValue = source.ElementAt(index);
+        var expected = Optional<RefType?>.Present(expectedValue);
+
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    [TestCase(0)]
+    [TestCase(4)]
+    [TestCase(int.MaxValue)]
+    public void ElementAtOrAbsentByIndexFromEnd_CollectionIndexIsNotInRange_ExpectAbsent(
+        int indexValue)
+    {
+        var index = Index.FromEnd(indexValue);
+
+        var source = CreateCollection(NullTextStructType, SomeTextStructType, default);
+
+        var actual = source.ElementAtOrAbsent(index);
+        var expected = Optional<StructType>.Absent;
+
+        Assert.AreEqual(expected, actual);
+    }
+}
