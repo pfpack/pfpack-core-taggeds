9cab9e3b0e57d466ecf19baf3e0e7917b262eaeb
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-03-16 22:17:22 +0400

    Result Flow: Redesign Forward impl

---
+++ src/core-taggeds-result/Result/Result.T/Result.T.Flow.Forward.Async.Value.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace System;
+
+partial struct Result<TSuccess, TFailure>
+{
+    public ValueTask<Result<TNextSuccess, TNextFailure>> ForwardValueAsync<TNextSuccess, TNextFailure>(
+        Func<TSuccess, ValueTask<Result<TNextSuccess, TNextFailure>>> nextFactoryAsync,
+        Func<TFailure, ValueTask<TNextFailure>> mapFailureAsync)
+        where TNextFailure : struct
+        =>
+        InnerForwardValueAsync(
+            nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync)),
+            mapFailureAsync ?? throw new ArgumentNullException(nameof(mapFailureAsync)));
+
+    public ValueTask<Result<TNextSuccess, TFailure>> ForwardValueAsync<TNextSuccess>(
+        Func<TSuccess, ValueTask<Result<TNextSuccess, TFailure>>> nextFactoryAsync)
+        =>
+        InnerForwardValueAsync(
+            nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync)));
+
+    public ValueTask<Result<TSuccess, TFailure>> ForwardValueAsync(
+        Func<TSuccess, ValueTask<Result<TSuccess, TFailure>>> nextFactoryAsync)
+        =>
+        InnerForwardValueAsync(
+            nextFactoryAsync ?? throw new ArgumentNullException(nameof(nextFactoryAsync)));
+}
---
+++ src/core-taggeds-result/Result/Result.T/Result.T.Flow.Forward.Sync.cs
@@ -0,0 +1,25 @@
+﻿namespace System;
+
+partial struct Result<TSuccess, TFailure>
+{
+    public Result<TNextSuccess, TNextFailure> Forward<TNextSuccess, TNextFailure>(
+        Func<TSuccess, Result<TNextSuccess, TNextFailure>> nextFactory,
+        Func<TFailure, TNextFailure> mapFailure)
+        where TNextFailure : struct
+        =>
+        InnerForward(
+            nextFactory ?? throw new ArgumentNullException(nameof(nextFactory)),
+            mapFailure ?? throw new ArgumentNullException(nameof(mapFailure)));
+
+    public Result<TNextSuccess, TFailure> Forward<TNextSuccess>(
+        Func<TSuccess, Result<TNextSuccess, TFailure>> nextFactory)
+        =>
+        InnerForward(
+            nextFactory ?? throw new ArgumentNullException(nameof(nextFactory)));
+
+    public Result<TSuccess, TFailure> Forward(
+        Func<TSuccess, Result<TSuccess, TFailure>> nextFactory)
+        =>
+        InnerForward(
+            nextFactory ?? throw new ArgumentNullException(nameof(nextFactory)));
+}
