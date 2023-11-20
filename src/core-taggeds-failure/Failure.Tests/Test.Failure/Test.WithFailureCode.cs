08c57efda7df2f2014e934b5a11c79429f3e5900
pmosk <pm@ggroupp.ru>
2023-11-21 00:30:20 +0400

    Impl WithFailureCode methods in Core.Failure

---
+++ src/core-taggeds-failure/Failure.Tests/Test.Failure/Test.WithFailureCode.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.Core.Tests.AssertHelper;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class FailureTest
+{
+    [Theory]
+    [InlineData(null, TestData.EmptyString)]
+    [InlineData(TestData.EmptyString, TestData.EmptyString)]
+    [InlineData(TestData.SomeString, TestData.SomeString)]
+    public static void WithFailureCodeNext_SourceExceptionIsNull_ExpectFailureCodeIsNextAndFailureMessageIsSame(
+        string? sourceFailureMessage, string expectedFailureMessage)
+    {
+        var source = new Failure<int>(TestData.PlusFifteen, sourceFailureMessage);
+        var actual = source.WithFailureCode(byte.MaxValue);
+
+        AssertEqualFailures(
+            (byte.MaxValue, expectedFailureMessage, null),
+            (actual.FailureCode, actual.FailureMessage, actual.SourceException));
+    }
+
+    [Theory]
+    [InlineData(null, TestData.EmptyString)]
+    [InlineData(TestData.EmptyString, TestData.EmptyString)]
+    [InlineData(TestData.SomeString, TestData.SomeString)]
+    public static void WithFailureCodeNext_SourceExceptionIsNotNull_ExpectFailureCodeIsNextAndFailureMessageIsSameAndSourceExceptionIsSame(
+        string? sourceFailureMessage, string expectedFailureMessage)
+    {
+        var sourceException = new Exception("Some exception message");
+
+        var source = new Failure<DateTimeKind>(DateTimeKind.Local, sourceFailureMessage)
+        {
+            SourceException = sourceException
+        };
+
+        var actual = source.WithFailureCode(UriKind.Absolute);
+
+        AssertEqualFailures(
+            (UriKind.Absolute, expectedFailureMessage, sourceException),
+            (actual.FailureCode, actual.FailureMessage, actual.SourceException));
+    }
+
+    [Theory]
+    [InlineData(null, TestData.EmptyString)]
+    [InlineData(TestData.EmptyString, TestData.EmptyString)]
+    [InlineData(TestData.SomeString, TestData.SomeString)]
+    public static void WithFailureCode_SourceExceptionIsNull_ExpectFailureCodeIsNextAndFailureMessageIsSame(
+        string? sourceFailureMessage, string expectedFailureMessage)
+    {
+        var source = new Failure<UriKind>(UriKind.Absolute, sourceFailureMessage);
+        var actual = source.WithFailureCode(UriKind.Relative);
+
+        AssertEqualFailures(
+            (UriKind.Relative, expectedFailureMessage, null),
+            (actual.FailureCode, actual.FailureMessage, actual.SourceException));
+    }
+
+    [Theory]
+    [InlineData(null, TestData.EmptyString)]
+    [InlineData(TestData.EmptyString, TestData.EmptyString)]
+    [InlineData(TestData.SomeString, TestData.SomeString)]
+    public static void WithFailureCode_SourceExceptionIsNotNull_ExpectFailureCodeIsNextAndFailureMessageIsSameAndSourceExceptionIsSame(
+        string? sourceFailureMessage, string expectedFailureMessage)
+    {
+        var sourceException = new Exception("Some error message");
+
+        var source = new Failure<decimal>(decimal.MaxValue, sourceFailureMessage)
+        {
+            SourceException = sourceException
+        };
+
+        var actual = source.WithFailureCode(decimal.One);
+
+        AssertEqualFailures(
+            (decimal.One, expectedFailureMessage, sourceException),
+            (actual.FailureCode, actual.FailureMessage, actual.SourceException));
+    }
+}
---
+++ src/core-taggeds-failure/Failure/Failure.T/Failure.T.WithFailureCode.cs
@@ -0,0 +1,27 @@
+﻿namespace System;
+
+partial struct Failure<TFailureCode>
+{
+    public Failure<TNextFailureCode> WithFailureCode<TNextFailureCode>(
+        TNextFailureCode nextFailureCode)
+        where TNextFailureCode : struct
+        =>
+        new(
+            failureCode: nextFailureCode,
+            failureMessage: failureMessage,
+            default)
+        {
+            SourceException = SourceException
+        };
+
+    public Failure<TFailureCode> WithFailureCode(
+        TFailureCode nextFailureCode)
+        =>
+        new(
+            failureCode: nextFailureCode,
+            failureMessage: failureMessage,
+            default)
+        {
+            SourceException = SourceException
+        };
+}
