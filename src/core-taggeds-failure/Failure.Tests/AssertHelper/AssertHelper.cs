508d741f49f85e9da231ad3b2c4cc19efdde1b59
Andrei Sergeev <a.sergeev.official@gmail.com>
2024-02-11 22:42:19 +0400

    Tests: minor refactor

--- src/core-taggeds-failure/Failure.Tests/AssertHelper/AssertHelper.cs
+++ src/core-taggeds-failure/Failure.Tests/AssertHelper/AssertHelper.cs
@@ -5,7 +5,7 @@ namespace PrimeFuncPack.Core.Tests;
 
 internal static class AssertHelper
 {
-    public static void AssertEqualFailures<TFailureCode>(
+    internal static void AssertEqualFailures<TFailureCode>(
         (TFailureCode FailureCode, string FailureMessage, Exception? SourceException) expected,
         (TFailureCode FailureCode, string FailureMessage, Exception? SourceException) actual)
         where TFailureCode : struct
