107e7b2cb728017fce7a4ef4f8c8b67514e233cc
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-01-12 16:11:53 +0400

    Simplify 'Contains' Equality

---
+++ src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.Contains.Wrap.cs
@@ -0,0 +1,30 @@
+﻿namespace System;
+
+partial struct Optional<T>
+{
+    // TODO: Add the tests and open the methods/operators
+
+    internal static bool Equals(Optional<T> left, T right)
+        =>
+        left.Contains(right);
+
+    //public static bool operator ==(Optional<T> left, T right)
+    //    =>
+    //    left.Contains(right);
+
+    //public static bool operator !=(Optional<T> left, T right)
+    //    =>
+    //    left.Contains(right) is not true;
+
+    internal static bool Equals(T left, Optional<T> right)
+        =>
+        right.Contains(left);
+
+    //public static bool operator ==(T left, Optional<T> right)
+    //    =>
+    //    right.Contains(left);
+
+    //public static bool operator !=(T left, Optional<T> right)
+    //    =>
+    //    right.Contains(left) is not true;
+}
