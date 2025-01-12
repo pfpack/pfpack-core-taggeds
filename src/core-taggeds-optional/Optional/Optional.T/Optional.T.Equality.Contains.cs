7566a1a311f7c14567db9a44125dbc1598c1d6a7
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-01-12 15:15:36 +0400

    Implement 'Contains' Equality

---
+++ src/core-taggeds-optional/Optional/Optional.T/Optional.T.Equality.Contains.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace System;
+
+partial struct Optional<T>
+{
+    // TODO: Add the tests and open the methods/operators
+
+    internal bool Contains(T value, IEqualityComparer<T>? comparer)
+        =>
+        InnerContains(value, comparer ?? EqualityComparer<T>.Default);
+
+    internal bool Contains(T value)
+        =>
+        InnerContains(value, EqualityComparer<T>.Default);
+
+    internal static bool Equals(Optional<T> left, T right, IEqualityComparer<T>? comparer)
+        =>
+        left.Contains(right, comparer);
+
+    internal static bool Equals(Optional<T> left, T right)
+        =>
+        left.Contains(right);
+
+    //public static bool operator ==(Optional<T> left, T right)
+    //    =>
+    //    left.Contains(right);
+
+    //public static bool operator !=(Optional<T> left, T right)
+    //    =>
+    //    left.Contains(right) is not true;
+
+    internal static bool Equals(T left, Optional<T> right, IEqualityComparer<T>? comparer)
+        =>
+        right.Contains(left, comparer);
+
+    internal static bool Equals(T left, Optional<T> right)
+        =>
+        right.Contains(left);
+
+    //public static bool operator ==(T left, Optional<T> right)
+    //    =>
+    //    right.Contains(left);
+
+    //public static bool operator !=(T left, Optional<T> right)
+    //    =>
+    //    right.Contains(left) is not true;
+}
---
+++ src/core-taggeds-optional/Optional/Optional/Optional.Equality.Contains.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace System;
+
+partial class Optional
+{
+    // TODO: Add the tests and open the methods
+
+    internal static bool Equals<T>(Optional<T> left, T right, IEqualityComparer<T>? comparer)
+        =>
+        left.Contains(right, comparer);
+
+    internal static bool Equals<T>(Optional<T> left, T right)
+        =>
+        left.Contains(right);
+
+    internal static bool Equals<T>(T left, Optional<T> right, IEqualityComparer<T>? comparer)
+        =>
+        right.Contains(left, comparer);
+
+    internal static bool Equals<T>(T left, Optional<T> right)
+        =>
+        right.Contains(left);
+}
