824756066bc4cd2bb9b055052d335315a0217d97
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-12-24 22:23:32 +0400

    Revert comments

--- src/core-taggeds-failure/Failure/Failure.T/T.Exception.To.cs
+++ src/core-taggeds-failure/Failure/Failure.T/T.Exception.To.cs
@@ -2,8 +2,6 @@
 
 partial struct Failure<TFailureCode>
 {
-    // We normalize empty message to null to get the default Exception message
-
     public Exception ToException()
         =>
         new(
